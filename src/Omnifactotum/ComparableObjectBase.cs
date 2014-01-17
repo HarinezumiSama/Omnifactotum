1883b34c66c1449e43ce5786adfd70599ff3e83e
vmcl <none@none>
2014-01-16 19:06:45 -0500

    Introduced: EquatableObjectBase and ComparableObjectBase

---
+++ src/Omnifactotum/ComparableObjectBase.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Omnifactotum.Annotations;
+
+namespace Omnifactotum
+{
+    /// <summary>
+    ///     Represents the abstract container that supports comparison.
+    /// </summary>
+    public abstract class ComparableObjectBase : EquatableObjectBase, IComparable<ComparableObjectBase>
+    {
+        #region Operators
+
+        /// <summary>
+        ///     Determines whether the left <see cref="ComparableObjectBase"/> instance is less than
+        ///     the right <see cref="ComparableObjectBase"/> instance.
+        /// </summary>
+        /// <param name="left">
+        ///     The first <see cref="ComparableObjectBase"/> instance to compare.
+        /// </param>
+        /// <param name="right">
+        ///     The second <see cref="ComparableObjectBase"/> instance to compare.
+        /// </param>
+        /// <returns>
+        ///     <b>true</b> if the left <see cref="ComparableObjectBase"/> instance is less than
+        ///     the right <see cref="ComparableObjectBase"/> instance; otherwise, <b>false</b>.
+        /// </returns>
+        public static bool operator <([CanBeNull] ComparableObjectBase left, [CanBeNull] ComparableObjectBase right)
+        {
+            return Comparer<ComparableObjectBase>.Default.Compare(left, right) < 0;
+        }
+
+        /// <summary>
+        ///     Determines whether the left <see cref="ComparableObjectBase"/> instance is less than or equal
+        ///     to the right <see cref="ComparableObjectBase"/> instance.
+        /// </summary>
+        /// <param name="left">
+        ///     The first <see cref="ComparableObjectBase"/> instance to compare.
+        /// </param>
+        /// <param name="right">
+        ///     The second <see cref="ComparableObjectBase"/> instance to compare.
+        /// </param>
+        /// <returns>
+        ///     <b>true</b> if the left <see cref="ComparableObjectBase"/> instance is less than or equal to
+        ///     the right <see cref="ComparableObjectBase"/> instance; otherwise, <b>false</b>.
+        /// </returns>
+        public static bool operator <=([CanBeNull] ComparableObjectBase left, [CanBeNull] ComparableObjectBase right)
+        {
+            return Comparer<ComparableObjectBase>.Default.Compare(left, right) <= 0;
+        }
+
+        /// <summary>
+        ///     Determines whether the left <see cref="ComparableObjectBase"/> instance is greater than
+        ///     the right <see cref="ComparableObjectBase"/> instance.
+        /// </summary>
+        /// <param name="left">
+        ///     The first <see cref="ComparableObjectBase"/> instance to compare.
+        /// </param>
+        /// <param name="right">
+        ///     The second <see cref="ComparableObjectBase"/> instance to compare.
+        /// </param>
+        /// <returns>
+        ///     <b>true</b> if the left <see cref="ComparableObjectBase"/> instance is greater than
+        ///     the right <see cref="ComparableObjectBase"/> instance; otherwise, <b>false</b>.
+        /// </returns>
+        public static bool operator >([CanBeNull] ComparableObjectBase left, [CanBeNull] ComparableObjectBase right)
+        {
+            return Comparer<ComparableObjectBase>.Default.Compare(left, right) > 0;
+        }
+
+        /// <summary>
+        ///     Determines whether the left <see cref="ComparableObjectBase"/> instance is greater than or equal
+        ///     to the right <see cref="ComparableObjectBase"/> instance.
+        /// </summary>
+        /// <param name="left">
+        ///     The first <see cref="ComparableObjectBase"/> instance to compare.
+        /// </param>
+        /// <param name="right">
+        ///     The second <see cref="ComparableObjectBase"/> instance to compare.
+        /// </param>
+        /// <returns>
+        ///     <b>true</b> if the left <see cref="ComparableObjectBase"/> instance is greater than or equal to
+        ///     the right <see cref="ComparableObjectBase"/> instance; otherwise, <b>false</b>.
+        /// </returns>
+        public static bool operator >=([CanBeNull] ComparableObjectBase left, [CanBeNull] ComparableObjectBase right)
+        {
+            return Comparer<ComparableObjectBase>.Default.Compare(left, right) >= 0;
+        }
+
+        #endregion
+
+        #region IComparable<ComparableObjectBase> Members
+
+        /// <summary>
+        ///     Compares the current object with another object of the same type.
+        /// </summary>
+        /// <param name="other">
+        ///     An object to compare with this object.
+        /// </param>
+        /// <returns>
+        ///     A value that indicates the relative order of the objects being compared.
+        ///     See <see cref="IComparable{T}.CompareTo"/>.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        ///     The <paramref name="other"/> object's type differs from this object's type.
+        /// </exception>
+        public int CompareTo([CanBeNull] ComparableObjectBase other)
+        {
+            if (ReferenceEquals(other, null))
+            {
+                return 1;
+            }
+
+            if (other.GetType() != GetType())
+            {
+                throw new ArgumentException("Incompatible comparand type.");
+            }
+
+            return CompareToInternal(other);
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        /// <summary>
+        ///     <para>
+        ///         Determines whether the current object is equal to another object of the same type.
+        ///     </para>
+        ///     <para>
+        ///         <b>IMPORTANT NOTE</b>: Default implementation of this method simply calls
+        ///         the <see cref="ComparableObjectBase.CompareToInternal"/> method, which may not always be
+        ///         the best option from the performance perspective.
+        ///     </para>
+        /// </summary>
+        /// <param name="other">
+        ///     An object to compare with this object. The parameter is checked prior to calling this method, therefore
+        ///     it is never <b>null</b> and can also be safely cast to an actual derived type.
+        /// </param>
+        /// <returns>
+        ///     <b>true</b> if the current object is equal to the <paramref name="other"/> parameter;
+        ///     otherwise, <b>false</b>.
+        /// </returns>
+        protected override bool EqualsInternal(EquatableObjectBase other)
+        {
+            return CompareToInternal((ComparableObjectBase)other) == 0;
+        }
+
+        /// <summary>
+        ///     Compares the current object with another object of the same type.
+        /// </summary>
+        /// <param name="other">
+        ///     An object to compare with this object.
+        /// </param>
+        /// <returns>
+        ///     A value that indicates the relative order of the objects being compared.
+        ///     See <see cref="IComparable{T}.CompareTo"/>.
+        /// </returns>
+        protected abstract int CompareToInternal([NotNull] ComparableObjectBase other);
+
+        #endregion
+    }
+}
---
+++ src/Omnifactotum/EquatableObjectBase.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Omnifactotum.Annotations;
+
+namespace Omnifactotum
+{
+    /// <summary>
+    ///     Represents the abstract container that supports comparison for equality.
+    /// </summary>
+    public abstract class EquatableObjectBase : IEquatable<EquatableObjectBase>
+    {
+        #region Operators
+
+        /// <summary>
+        ///     Determines whether the two specified <see cref="EquatableObjectBase"/> instances are equal.
+        /// </summary>
+        /// <param name="left">
+        ///     The first <see cref="EquatableObjectBase"/> instance to compare.
+        /// </param>
+        /// <param name="right">
+        ///     The second <see cref="EquatableObjectBase"/> instance to compare.
+        /// </param>
+        /// <returns>
+        ///     <b>true</b> if the two specified <see cref="EquatableObjectBase"/> instances are equal;
+        ///     otherwise, <b>false</b>.
+        /// </returns>
+        public static bool operator ==([CanBeNull] EquatableObjectBase left, [CanBeNull] EquatableObjectBase right)
+        {
+            return EqualityComparer<EquatableObjectBase>.Default.Equals(left, right);
+        }
+
+        /// <summary>
+        ///     Determines whether the two specified <see cref="EquatableObjectBase"/> instances are not equal.
+        /// </summary>
+        /// <param name="left">
+        ///     The first <see cref="EquatableObjectBase"/> instance to compare.
+        /// </param>
+        /// <param name="right">
+        ///     The second <see cref="EquatableObjectBase"/> instance to compare.
+        /// </param>
+        /// <returns>
+        ///     <b>true</b> if the two specified <see cref="EquatableObjectBase"/> instances are not equal;
+        ///     otherwise, <b>false</b>.
+        /// </returns>
+        public static bool operator !=([CanBeNull] EquatableObjectBase left, [CanBeNull] EquatableObjectBase right)
+        {
+            return !(left == right);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        ///     Determines whether the specified <see cref="Object"/> is equal to
+        ///     this <see cref="EquatableObjectBase"/>.
+        /// </summary>
+        /// <param name="obj">
+        ///     The <see cref="Object"/> to compare with this instance.
+        /// </param>
+        /// <returns>
+        ///     <b>true</b> if the specified <see cref="System.Object"/> is equal to
+        ///     this <see cref="EquatableObjectBase"/>; otherwise, <b>false</b>.
+        /// </returns>
+        public sealed override bool Equals(object obj)
+        {
+            return Equals(obj as EquatableObjectBase);
+        }
+
+        /// <summary>
+        ///     Returns a hash code for this <see cref="EquatableObjectBase"/>.
+        /// </summary>
+        /// <returns>
+        ///     A hash code for this instance, suitable for use in hashing algorithms and data structures like
+        ///     a hash table.
+        /// </returns>
+        public sealed override int GetHashCode()
+        {
+            return GetHashCodeInternal();
+        }
+
+        #endregion
+
+        #region IEquatable<EquatableObjectBase> Members
+
+        /// <summary>
+        ///     Determines whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="other">
+        ///     An object to compare with this object.
+        /// </param>
+        /// <returns>
+        ///     <b>true</b> if the current object is equal to the <paramref name="other"/> parameter;
+        ///     otherwise, <b>false</b>.
+        /// </returns>
+        public bool Equals([CanBeNull] EquatableObjectBase other)
+        {
+            if (ReferenceEquals(other, null) || other.GetType() != GetType())
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(other, this))
+            {
+                return true;
+            }
+
+            return EqualsInternal(other);
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        /// <summary>
+        ///     Returns a hash code for this <see cref="EquatableObjectBase"/>.
+        /// </summary>
+        /// <returns>
+        ///     A hash code for this instance, suitable for use in hashing algorithms and data structures like
+        ///     a hash table.
+        /// </returns>
+        protected abstract int GetHashCodeInternal();
+
+        /// <summary>
+        ///     Determines whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="other">
+        ///     An object to compare with this object. The parameter is checked prior to calling this method, therefore
+        ///     it is never <b>null</b> in this method and also can be safely cast to an actual derived type.
+        /// </param>
+        /// <returns>
+        ///     <b>true</b> if the current object is equal to the <paramref name="other"/> parameter;
+        ///     otherwise, <b>false</b>.
+        /// </returns>
+        protected abstract bool EqualsInternal([NotNull] EquatableObjectBase other);
+
+        #endregion
+    }
+}
