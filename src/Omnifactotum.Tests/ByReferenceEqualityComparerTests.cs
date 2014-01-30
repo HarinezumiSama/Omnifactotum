c7f090b9949258d1e81adca7788005de217f7dba
vmcl.nb <none@none>
2014-01-30 02:25:02 -0500

    [1] ByReferenceEqualityComparer tests [2] EnumHelper tests

---
+++ src/Omnifactotum.Tests/ByReferenceEqualityComparerTests.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Omnifactotum.Tests
+{
+    [TestFixture]
+    public sealed class ByReferenceEqualityComparerTests
+    {
+        #region Tests
+
+        [Test]
+        public void TestReferenceType()
+        {
+            Assert.That(typeof(SomeReferenceType).IsClass, Is.True);
+
+            var equalityComparer = ByReferenceEqualityComparer<SomeReferenceType>.Instance;
+
+            var objA1 = new SomeReferenceType { Value = "A" };
+            var objA2 = new SomeReferenceType { Value = "A" };
+            var objB = new SomeReferenceType { Value = "B" };
+
+            Assert.That(equalityComparer.Equals(objA1, objA1), Is.True);
+            Assert.That(equalityComparer.GetHashCode(objA1), Is.EqualTo(equalityComparer.GetHashCode(objA1)));
+
+            Assert.That(equalityComparer.Equals(objA1, objA2), Is.False);
+            Assert.That(equalityComparer.GetHashCode(objA1), Is.Not.EqualTo(equalityComparer.GetHashCode(objA2)));
+
+            Assert.That(equalityComparer.Equals(objA1, objB), Is.False);
+            Assert.That(equalityComparer.GetHashCode(objA1), Is.Not.EqualTo(equalityComparer.GetHashCode(objB)));
+        }
+
+        [Test]
+        public void TestValueType()
+        {
+            Assert.That(typeof(SomeValueType).IsValueType, Is.True);
+
+            var equalityComparer = ByReferenceEqualityComparer<SomeValueType>.Instance;
+
+            var objA1 = new SomeValueType { Value = "A" };
+            var objA2 = new SomeValueType { Value = "A" };
+            var objB = new SomeValueType { Value = "B" };
+
+            Assert.That(equalityComparer.Equals(objA1, objA1), Is.True);
+            Assert.That(equalityComparer.GetHashCode(objA1), Is.EqualTo(equalityComparer.GetHashCode(objA1)));
+
+            Assert.That(equalityComparer.Equals(objA1, objA2), Is.True);
+            Assert.That(equalityComparer.GetHashCode(objA1), Is.EqualTo(equalityComparer.GetHashCode(objA1)));
+
+            Assert.That(equalityComparer.Equals(objA1, objB), Is.False);
+            Assert.That(equalityComparer.GetHashCode(objA1), Is.Not.EqualTo(equalityComparer.GetHashCode(objB)));
+        }
+
+        #endregion
+
+        #region SomeReferenceType Class
+
+        private sealed class SomeReferenceType : IEquatable<SomeReferenceType>
+        {
+            #region Public Properties
+
+            public string Value
+            {
+                get;
+                set;
+            }
+
+            #endregion
+
+            #region Public Methods
+
+            public override bool Equals(object obj)
+            {
+                return Equals(obj as SomeReferenceType);
+            }
+
+            public override int GetHashCode()
+            {
+                return this.Value == null ? 0 : StringComparer.Ordinal.GetHashCode(this.Value);
+            }
+
+            #endregion
+
+            #region IEquatable<SomeReferenceType> Members
+
+            public bool Equals(SomeReferenceType other)
+            {
+                return other != null && StringComparer.Ordinal.Equals(this.Value, other.Value);
+            }
+
+            #endregion
+        }
+
+        #endregion
+
+        #region SomeValueType Class
+
+        private struct SomeValueType : IEquatable<SomeValueType>
+        {
+            #region Public Properties
+
+            public string Value
+            {
+                get;
+                set;
+            }
+
+            #endregion
+
+            #region Public Methods
+
+            public override bool Equals(object obj)
+            {
+                return obj is SomeValueType && Equals((SomeValueType)obj);
+            }
+
+            public override int GetHashCode()
+            {
+                return this.Value == null ? 0 : StringComparer.Ordinal.GetHashCode(this.Value);
+            }
+
+            #endregion
+
+            #region IEquatable<SomeReferenceType> Members
+
+            public bool Equals(SomeValueType other)
+            {
+                return StringComparer.Ordinal.Equals(this.Value, other.Value);
+            }
+
+            #endregion
+        }
+
+        #endregion
+    }
+}
---
+++ src/Omnifactotum.Tests/EnumHelperTests.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Omnifactotum.Annotations;
+
+namespace Omnifactotum.Tests
+{
+    [TestFixture]
+    public sealed class EnumHelperTests
+    {
+        #region Tests
+
+        [Test]
+        public void TestGetValue()
+        {
+            const FileMode EnumValue = FileMode.OpenOrCreate;
+            var enumValueString = EnumValue.ToString();
+            var enumValueStringLower = enumValueString.ToLowerInvariant();
+
+            Assert.That(() => EnumHelper.GetValue<int>(enumValueString), Throws.ArgumentException);
+
+            Assert.That(() => EnumHelper.GetValue<FileMode>(enumValueString), Is.EqualTo(EnumValue));
+            Assert.That(() => EnumHelper.GetValue<FileMode>(enumValueString, false), Is.EqualTo(EnumValue));
+            Assert.That(() => EnumHelper.GetValue<FileMode>(enumValueString, true), Is.EqualTo(EnumValue));
+
+            Assert.That(() => EnumHelper.GetValue<FileMode>(null), Throws.ArgumentException);
+            Assert.That(() => EnumHelper.GetValue<FileMode>(string.Empty), Throws.ArgumentException);
+            Assert.That(() => EnumHelper.GetValue<FileMode>(enumValueStringLower), Throws.ArgumentException);
+            Assert.That(() => EnumHelper.GetValue<FileMode>(enumValueStringLower, false), Throws.ArgumentException);
+            Assert.That(() => EnumHelper.GetValue<FileMode>(enumValueStringLower, true), Is.EqualTo(EnumValue));
+        }
+
+        [Test]
+        public void TestGetAllValues()
+        {
+            Assert.That(() => EnumHelper.GetAllValues<int>(), Throws.ArgumentException);
+
+            Assert.That(
+                () => EnumHelper.GetAllValues<FileMode>(),
+                Is.EquivalentTo(Enum.GetValues(typeof(FileMode))));
+
+            Assert.That(
+                () => EnumHelper.GetAllValues<FileAttributes>(),
+                Is.EquivalentTo(Enum.GetValues(typeof(FileAttributes))));
+        }
+
+        [Test]
+        public void TestGetAllFlagValues()
+        {
+            Assert.That(() => EnumHelper.GetAllFlagValues<int>(), Throws.ArgumentException);
+
+            Assert.That(() => EnumHelper.GetAllFlagValues<FileMode>(), Throws.ArgumentException);
+
+            Assert.That(
+                () => EnumHelper.GetAllFlagValues<FileAttributes>(),
+                Is.EquivalentTo(Enum.GetValues(typeof(FileAttributes))));
+
+            Assert.That(
+                () => EnumHelper.GetAllFlagValues<FileShare>(),
+                Is.EquivalentTo(
+                    Enum.GetValues(typeof(FileShare))
+                        .Cast<FileShare>()
+                        .Except(new[] { FileShare.None, FileShare.ReadWrite })));
+
+            Assert.That(
+                () => EnumHelper.GetAllFlagValues<FlagsInt64>(),
+                Is.EquivalentTo(new[] { FlagsInt64.Bit0, FlagsInt64.Bit32, FlagsInt64.Bit49, FlagsInt64.Bit63 }));
+
+            Assert.That(
+                () => EnumHelper.GetAllFlagValues<FlagsUInt64>(),
+                Is.EquivalentTo(new[] { FlagsUInt64.Bit0, FlagsUInt64.Bit35, FlagsUInt64.Bit53, FlagsUInt64.Bit63 }));
+        }
+
+        #endregion
+
+        #region FlagsInt64 Enumeration
+
+        [Flags]
+        private enum FlagsInt64 : long
+        {
+            [UsedImplicitly]
+            Bit0 = 1L << 0,
+
+            [UsedImplicitly]
+            Bit32 = 1L << 32,
+
+            [UsedImplicitly]
+            Bit49 = 1L << 49,
+
+            [UsedImplicitly]
+            Bit63 = 1L << 63,
+
+            [UsedImplicitly]
+            Mask1 = Bit0 | Bit63,
+
+            [UsedImplicitly]
+            All = unchecked((long)0xFFFFFFFFFFFFFFFFUL)
+        }
+
+        #endregion
+
+        #region FlagsUInt64 Enumeration
+
+        [Flags]
+        private enum FlagsUInt64 : ulong
+        {
+            [UsedImplicitly]
+            Bit0 = 1UL << 0,
+
+            [UsedImplicitly]
+            Bit35 = 1UL << 35,
+
+            [UsedImplicitly]
+            Bit53 = 1UL << 53,
+
+            [UsedImplicitly]
+            Bit63 = 1UL << 63,
+
+            [UsedImplicitly]
+            Mask1 = Bit0 | Bit63,
+
+            [UsedImplicitly]
+            All = 0xFFFFFFFFFFFFFFFFUL
+        }
+
+        #endregion
+    }
+}
