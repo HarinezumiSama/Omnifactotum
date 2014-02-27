a987d2a4189bf0c4cb71b0405ad164c9393afef1
vmcl <none@none>
2014-02-27 14:57:54 -0500

    Validation: NotNullOrWhiteSpaceStringConstraint - introduced

---
+++ src/Omnifactotum.Tests/NotNullOrWhiteSpaceStringConstraintTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using Omnifactotum.Validation.Constraints;
+
+namespace Omnifactotum.Tests
+{
+    [TestFixture]
+    public sealed class NotNullOrWhiteSpaceStringConstraintTests : ConstraintTestsBase
+    {
+        #region Tests
+
+        [Test]
+        public void TestValidateNegativeContextNull()
+        {
+            var constraint = new NotNullOrWhiteSpaceStringConstraint();
+
+            Assert.That(() => constraint.Validate(null, "A"), Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void TestValidateNegativeInvalidValueType()
+        {
+            var constraint = new NotNullOrWhiteSpaceStringConstraint();
+            var validationContext = CreateTestValidationContext();
+
+            Assert.That(
+                () => constraint.Validate(validationContext, this),
+                Throws.TypeOf<InvalidOperationException>());
+        }
+
+        [Test]
+        [TestCase("A")]
+        public void TestValidateAgainstNotNullOrEmptyOrWhiteSpace(string value)
+        {
+            var constraint = new NotNullOrWhiteSpaceStringConstraint();
+            var validationContext = CreateTestValidationContext();
+            var validationError = constraint.Validate(validationContext, value);
+
+            Assert.That(validationError, Is.Null);
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void TestValidateAgainstInvalidValue(string value)
+        {
+            var constraint = new NotNullOrWhiteSpaceStringConstraint();
+            var validationContext = CreateTestValidationContext();
+            var validationError = constraint.Validate(validationContext, value);
+
+            Assert.That(validationError, Is.Not.Null);
+            Assert.That(validationError.FailedConstraintType, Is.EqualTo(constraint.GetType()));
+            Assert.That(validationError.Context, Is.SameAs(validationContext));
+        }
+
+        #endregion
+    }
+}
---
+++ src/Omnifactotum/Validation/Constraints/NotNullOrWhiteSpaceStringConstraint.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace Omnifactotum.Validation.Constraints
+{
+    /// <summary>
+    ///     Specifies that the annotated member of type <see cref="String"/> should not be <b>null</b> or empty.
+    /// </summary>
+    public sealed class NotNullOrWhiteSpaceStringConstraint : MemberConstraintBase
+    {
+        #region Protected Methods
+
+        /// <summary>
+        ///     Validates the specified value is scope of the specified context.
+        /// </summary>
+        /// <param name="context">
+        ///     The context of validation.
+        /// </param>
+        /// <param name="value">
+        ///     The value to validate.
+        /// </param>
+        /// <returns>
+        ///     <b>null</b> if validation succeeded; or a <see cref="MemberConstraintValidationError"/> instance
+        ///     describing the validation error, if validation failed.
+        /// </returns>
+        protected override MemberConstraintValidationError ValidateInternal(
+            MemberConstraintValidationContext context,
+            object value)
+        {
+            var stringValue = CastTo<string>(value);
+            if (!string.IsNullOrWhiteSpace(stringValue))
+            {
+                return null;
+            }
+
+            return CreateError(context, "The value must not be null or an empty string or a whitespace-only string.");
+        }
+
+        #endregion
+    }
+}
