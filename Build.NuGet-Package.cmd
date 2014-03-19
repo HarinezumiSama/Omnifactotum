a352944a1225a505eedafa09895804edfa65733f
vmcl <none@none>
2014-03-19 16:36:30 -0400

    Introduced the script for NuGet (Build.NuGet-Package.cmd)

---
+++ Build.NuGet-Package.cmd
@@ -0,0 +1,30 @@
+@echo off
+
+setlocal
+
+set EnableNuGetPackageRestore=true
+
+set SRC_PROJECT_PATH=%~dp0\src\Omnifactotum\Omnifactotum.csproj
+set NG_PROJECT_PATH=%SRC_PROJECT_PATH%
+set PKG_PATH=%~dp0\bin\NuGet
+
+echo Building project...
+"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" "%SRC_PROJECT_PATH%" /target:Rebuild /p:Configuration=Release /p:Platform=AnyCPU || exit /b 1
+echo Building project - DONE.
+
+echo.
+echo (Re)creating directory for the package '%PKG_PATH%'...
+if exist "%PKG_PATH%" (
+    rd /s /q "%PKG_PATH%" || exit /b 2
+)
+md "%PKG_PATH%" || exit /b 3
+echo (Re)creating directory for the package - DONE.
+
+echo.
+echo Creating package...
+:: [vitalii.maklai] The backslash (\) before the double quote (") must be escaped with itself (\) for the command line parameter to be parsed properly
+nuget pack "%NG_PROJECT_PATH%" -Verbosity detailed -OutputDirectory "%PKG_PATH%\\" -BasePath "%~dp0\src\Omnifactotum" -Properties Configuration=Release;Platform=AnyCPU -Exclude *\*.txt || exit /b 4
+echo Creating package - DONE
+
+echo.
+echo * FINISHED.
