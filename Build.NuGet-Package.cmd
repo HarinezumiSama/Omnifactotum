10a2fe34469965c02461f2f4e2b4883c46edae8d
vmcl <none@none>
2014-12-10 11:09:36 -0500

    Improved the "Build.NuGet-Package" script: [1] Better logging and error handling [2] Running the tests before creating a NuGet package

--- Build.NuGet-Package.cmd
+++ Build.NuGet-Package.cmd
@@ -1,20 +1,26 @@
 @echo off
 
+:: TODO: Convert this script to a PowerShell script
+
+setlocal
+
 verify invalid params >nul
 setlocal enableextensions enabledelayedexpansion
 if errorlevel 1 goto EXT_ERROR
 
-if "%SIGN_OMNIFACTOTUM%" neq "1" (
-    echo *** ERROR: The assembly signing is not enabled. The NuGet package will not be created.
-    exit /b 100
-)
-
 set EnableNuGetPackageRestore=true
 
 set SRC_PROJECT_PATH=%~dp0\src\Omnifactotum\Omnifactotum.csproj
+set SRC_SOLUTION_PATH=%~dp0\src\Omnifactotum.sln
 set REL_NOTES_PATH=%~dp0\src\Omnifactotum.ReleaseNotes.txt
 set NG_PROJECT_PATH=%SRC_PROJECT_PATH%
+set OUT_DIR=%~dp0\bin\AnyCpu\Release
 set PKG_PATH=%~dp0\bin\NuGet
+
+if "%NUnitBinDir%" equ "" set NUnitBinDir=C:\Program Files (x86)\NUnit\bin
+set NUNIT_CONSOLE=%NUnitBinDir%\nunit-console.exe
+
+if not exist "%NUNIT_CONSOLE%" (echo * ERROR: NUnit Console executable is not found at "%NUNIT_CONSOLE%". ^(Is the NUnitBinDir environment variable properly set^?^) & goto ERROR)
 
 (set LF=^
 
@@ -33,27 +39,44 @@ if exist "!REL_NOTES_PATH!" (
 ::echo !PkgReleaseNotes!
 ::goto :EOF
 
-echo Building project...
-"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" "%SRC_PROJECT_PATH%" /target:Rebuild /p:Configuration=Release /p:Platform=AnyCPU || exit /b 1
-echo Building project - DONE.
-
-:: TODO: Run unit tests before creating package
+echo * Cleaning the output directory...
+if exist "%OUT_DIR%" (
+    rd /s /q "%OUT_DIR%" || goto ERROR
+)
+echo * Cleaning the output directory - DONE.
 
 echo.
-echo (Re)creating directory for the package '%PKG_PATH%'...
-if exist "%PKG_PATH%" (
-    rd /s /q "%PKG_PATH%" || exit /b 2
+echo * Building project...
+"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" "%SRC_SOLUTION_PATH%" /target:Rebuild /p:Configuration="Release" /p:Platform="Any CPU" || goto ERROR
+echo * Building project - DONE.
+
+echo.
+echo * Running automated tests...
+"%NUNIT_CONSOLE%" "%OUT_DIR%\Tests\Omnifactotum.Tests.dll" /basepath="%OUT_DIR%\Tests" /work="%OUT_DIR%\Tests" /labels /stoponerror /noshadow || goto ERROR
+echo * Running automated tests - DONE.
+
+
+if "%SIGN_OMNIFACTOTUM%" neq "1" (
+    echo.
+    echo *** ERROR: The assembly signing is not enabled. The NuGet package will not be created.
+    goto ERROR
 )
-md "%PKG_PATH%" || exit /b 3
-echo (Re)creating directory for the package - DONE.
+
+echo.
+echo * (Re)creating directory for the package '%PKG_PATH%'...
+if exist "%PKG_PATH%" (
+    rd /s /q "%PKG_PATH%" || goto ERROR
+)
+md "%PKG_PATH%" || goto ERROR
+echo * (Re)creating directory for the package - DONE.
 
 :: ----------
 
 echo.
-echo Creating package...
+echo * Creating package...
 :: [vitalii.maklai] The backslash (\) before the double quote (") must be escaped with itself (\) for the command line parameters to be parsed properly
-nuget pack "%NG_PROJECT_PATH%" -Verbosity detailed -OutputDirectory "%PKG_PATH%\\" -Symbols -Properties Configuration=Release;Platform=AnyCPU;PkgReleaseNotes="!PkgReleaseNotes!" || exit /b 4
-echo Creating package - DONE
+nuget pack "%NG_PROJECT_PATH%" -Verbosity detailed -OutputDirectory "%PKG_PATH%\\" -Symbols -Properties Configuration=Release;Platform=AnyCPU;PkgReleaseNotes="!PkgReleaseNotes!" || goto ERROR
+echo * Creating package - DONE.
 
 echo.
 echo * FINISHED.
@@ -74,4 +97,4 @@ echo. *** ERROR ***
 if /i "%~1" equ "" (
     pause
 )
-exit /b 100
+exit /b 1
