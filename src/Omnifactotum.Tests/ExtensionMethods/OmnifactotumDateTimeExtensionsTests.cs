c47d4b8024c20f7d656b47051fb7f000fc2e2bb4
vmcl <none@none>
2014-12-29 23:03:09 -0500

    Added the unit tests for the OmnifactotumDateTimeExtensions and OmnifactotumDateTimeOffsetExtensions classes

---
+++ src/Omnifactotum.Tests/ExtensionMethods/OmnifactotumDateTimeExtensionsTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Omnifactotum.Tests.ExtensionMethods
+{
+    [TestFixture]
+    public sealed class OmnifactotumDateTimeExtensionsTests
+    {
+        #region Tests
+
+        [Test]
+        [TestCase(DateTimeKind.Local)]
+        [TestCase(DateTimeKind.Unspecified)]
+        [TestCase(DateTimeKind.Utc)]
+        public void TestToFixedString(DateTimeKind kind)
+        {
+            var dateTime = new DateTime(2001, 2, 3, 7, 8, 9, 123);
+
+            var actualResult = dateTime.ToFixedString();
+            Assert.That(actualResult, Is.EqualTo("2001-02-03 07:08:09"));
+        }
+
+        [Test]
+        [TestCase(DateTimeKind.Local)]
+        [TestCase(DateTimeKind.Unspecified)]
+        [TestCase(DateTimeKind.Utc)]
+        public void TestToPreciseFixedString(DateTimeKind kind)
+        {
+            var dateTime = new DateTime(2001, 2, 3, 7, 8, 9, 123);
+
+            var actualResult = dateTime.ToPreciseFixedString();
+            Assert.That(actualResult, Is.EqualTo("2001-02-03 07:08:09.1230000"));
+        }
+
+        #endregion
+    }
+}
---
+++ src/Omnifactotum.Tests/ExtensionMethods/OmnifactotumDateTimeOffsetExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Omnifactotum.Tests.ExtensionMethods
+{
+    [TestFixture]
+    public sealed class OmnifactotumDateTimeOffsetExtensionsTests
+    {
+        #region Tests
+
+        [Test]
+        public void TestToFixedString()
+        {
+            var dateTimeOffset = new DateTimeOffset(2001, 2, 3, 7, 8, 9, 123, new TimeSpan(2, 30, 0));
+
+            var actualResult = dateTimeOffset.ToFixedString();
+            Assert.That(actualResult, Is.EqualTo("2001-02-03 07:08:09 UTC+02:30"));
+        }
+
+        [Test]
+        public void TestToPreciseFixedString()
+        {
+            var dateTimeOffset = new DateTimeOffset(2001, 2, 3, 7, 8, 9, 123, new TimeSpan(2, 30, 0));
+
+            var actualResult = dateTimeOffset.ToPreciseFixedString();
+            Assert.That(actualResult, Is.EqualTo("2001-02-03 07:08:09.1230000 UTC+02:30"));
+        }
+
+        #endregion
+    }
+}
