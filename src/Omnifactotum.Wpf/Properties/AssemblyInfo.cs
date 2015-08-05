2ae9c9c126a724e41ff68e8bff5a54901d4f94a1
vmcl <none@none>
2015-08-05 06:18:46 -0400

    Moved Omnifactotum.Wpf to https://omnifactotumwpf.codeplex.com/

--- src/Omnifactotum.Wpf/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Omnifactotum WPF")]
-[assembly: CLSCompliant(true)]
-[assembly: Guid("45a781f2-ed60-4f4b-9eba-976ea2f9d4a4")]
