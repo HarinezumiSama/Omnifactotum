e591acf7073eb8f2bcc37397d08f2bc1cb24cdcc
HarinezumiSama <vitalii.maklai@gmail.com>
2017-10-26 22:08:00 -0400

    * OmnifactotumNullableBooleanExtensions: Added ToString overload accepting value provider delegates * Covered OmnifactotumNullableBooleanExtensions with unit tests

---
+++ src/Omnifactotum.Tests/ExtensionMethods/OmnifactotumNullableBooleanExtensionsTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+
+namespace Omnifactotum.Tests.ExtensionMethods
+{
+    [TestFixture(TestOf = typeof(OmnifactotumNullableBooleanExtensions))]
+    internal sealed class OmnifactotumNullableBooleanExtensionsTests
+    {
+        [Test]
+        [TestCase(null, "Dunno", "Yup", "Nope", "Dunno")]
+        [TestCase(null, null, "Yup", "Nope", null)]
+        [TestCase(null, "", "Yup", "Nope", "")]
+        [TestCase(true, "Dunno", "Yup", "Nope", "Yup")]
+        [TestCase(true, "Dunno", null, "Nope", null)]
+        [TestCase(true, "Dunno", "", "Nope", "")]
+        [TestCase(false, "Dunno", "Yup", "Nope", "Nope")]
+        [TestCase(false, "Dunno", "Yup", null, null)]
+        [TestCase(false, "Dunno", "Yup", "", "")]
+        public void TestToStringOverloadsSucceed(
+            bool? value,
+            string noValueString,
+            string trueValueString,
+            string falseValueString,
+            string expectedResult)
+        {
+            Assert.That(new[] { noValueString, trueValueString, falseValueString }, Is.Unique);
+
+            var actualResult1 = value.ToString(noValueString, trueValueString, falseValueString);
+            Assert.That(actualResult1, Is.EqualTo(expectedResult));
+
+            var actualResult2 = value.ToString(() => noValueString, () => trueValueString, () => falseValueString);
+            Assert.That(actualResult2, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        [SuppressMessage("ReSharper", "AssignNullToNotNullAttribute")]
+        public void TestToStringWithNullProviderThrows()
+        {
+            Assert.That(
+                () => ((bool?)null).ToString(null, () => "Yes", () => "No"),
+                Throws.ArgumentNullException
+                    .With
+                    .Property(nameof(ArgumentException.ParamName))
+                    .EqualTo("noValueProvider"));
+
+            Assert.That(
+                () => ((bool?)true).ToString(() => "Dunno", null, () => "No"),
+                Throws.ArgumentNullException
+                    .With
+                    .Property(nameof(ArgumentException.ParamName))
+                    .EqualTo("trueValueProvider"));
+
+            Assert.That(
+                () => ((bool?)false).ToString(() => "Dunno", () => "Yes", null),
+                Throws.ArgumentNullException
+                    .With
+                    .Property(nameof(ArgumentException.ParamName))
+                    .EqualTo("falseValueProvider"));
+        }
+    }
+}
--- src/Omnifactotum/ExtensionMethods/OmnifactotumNullableBooleanExtensions.cs
+++ src/Omnifactotum/ExtensionMethods/OmnifactotumNullableBooleanExtensions.cs
@@ -1,12 +1,12 @@
 ﻿using Omnifactotum.Annotations;
 
-//// Namespace is intentionally named so in order to simplify usage of extension methods
-//// ReSharper disable once CheckNamespace
+//// ReSharper disable once CheckNamespace - Namespace is intentionally named so in order to simplify usage of extension methods
+
 namespace System
 {
     /// <summary>
-    ///     Contains extension methods for
-    ///     the <see cref="System.Nullable{T}">System.Nullable</see>&lt;<see cref="System.Boolean"/>&gt; type.
+    ///     Contains extension methods for the
+    ///     <see cref="System.Nullable{T}">System.Nullable</see>&lt;<see cref="System.Boolean"/>&gt; type.
     /// </summary>
     public static class OmnifactotumNullableBooleanExtensions
     {
@@ -17,24 +17,54 @@ namespace System
         ///     The value to get the string representation of.
         /// </param>
         /// <param name="noValueString">
-        ///     Specifies the string to return if <paramref name="value"/> is <c>null</c>, that is, has no inner value.
+        ///     A string value to return when <paramref name="value"/> is <c>null</c>.
         /// </param>
         /// <param name="trueValueString">
-        ///     Specifies the string to return if <paramref name="value"/> is <c>true</c>.
+        ///     A string value to return when <paramref name="value"/> is <c>true</c>.
         /// </param>
         /// <param name="falseValueString">
-        ///     Specifies the string to return if <paramref name="value"/> is <c>false</c>.
+        ///     A string value to return when <paramref name="value"/> is <c>false</c>.
         /// </param>
         /// <returns>
         ///     The string representation of the specified nullable Boolean value.
         /// </returns>
         public static string ToString(
             [CanBeNull] this bool? value,
-            string noValueString,
-            string trueValueString,
-            string falseValueString)
-        {
-            return value.HasValue ? (value.Value ? trueValueString : falseValueString) : noValueString;
-        }
+            [CanBeNull] string noValueString,
+            [CanBeNull] string trueValueString,
+            [CanBeNull] string falseValueString)
+            => value.HasValue ? (value.Value ? trueValueString : falseValueString) : noValueString;
+
+        /// <summary>
+        ///     Gets the string representation of the specified nullable Boolean value.
+        /// </summary>
+        /// <param name="value">
+        ///     The value to get the string representation of.
+        /// </param>
+        /// <param name="noValueProvider">
+        ///     A reference to a method that provides a value to return when <paramref name="value"/>
+        ///     is <c>null</c>.
+        /// </param>
+        /// <param name="trueValueProvider">
+        ///     A reference to a method that provides a value to return when <paramref name="value"/>
+        ///     is <c>true</c>.
+        /// </param>
+        /// <param name="falseValueProvider">
+        ///     A reference to a method that provides a value to return when <paramref name="value"/>
+        ///     is <c>false</c>.
+        /// </param>
+        /// <returns>
+        ///     The string representation of the specified nullable Boolean value.
+        /// </returns>
+        public static string ToString(
+            [CanBeNull] this bool? value,
+            [NotNull] Func<string> noValueProvider,
+            [NotNull] Func<string> trueValueProvider,
+            [NotNull] Func<string> falseValueProvider)
+            => value.HasValue
+                ? (value.Value
+                    ? (trueValueProvider ?? throw new ArgumentNullException(nameof(trueValueProvider))).Invoke()
+                    : (falseValueProvider ?? throw new ArgumentNullException(nameof(falseValueProvider))).Invoke())
+                : (noValueProvider ?? throw new ArgumentNullException(nameof(noValueProvider))).Invoke();
     }
 }
