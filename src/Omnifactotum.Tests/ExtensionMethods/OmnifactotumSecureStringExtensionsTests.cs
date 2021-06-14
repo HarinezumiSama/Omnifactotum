2b5fbacbf8c8896b05f3b3a24c592c013d21e0a3
Vitalii Maklai <vitalii.maklai@gmail.com>
2021-06-13 20:33:59 -0400

    Added new extension methods for `System.String` and `System.Security.SecureString`:
    - `ToSecureString(string)`
    - `IsNullOrEmpty(SecureString)` and `ToPlainText(SecureString)`

---
+++ src/Omnifactotum.Tests/ExtensionMethods/OmnifactotumSecureStringExtensionsTests.cs
@@ -0,0 +1,77 @@
+﻿#nullable enable
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Security;
+using NUnit.Framework;
+
+namespace Omnifactotum.Tests.ExtensionMethods
+{
+    [TestFixture(TestOf = typeof(OmnifactotumSecureStringExtensions))]
+    internal sealed class OmnifactotumSecureStringExtensionsTests
+    {
+        [Test]
+        [TestCase(null, true)]
+        [TestCase("", true)]
+        [TestCase("\u0000", false)]
+        [TestCase("\u0020", false)]
+        [TestCase("\t", false)]
+        [TestCase("\r", false)]
+        [TestCase("\n", false)]
+        [TestCase(@"luNCh/обеД/ランチ/dÉJEuner/午餐", false)]
+        [SuppressMessage("ReSharper", "StringLiteralTypo")]
+        public void TestIsNullOrEmpty(string? plainText, bool expectedResult)
+        {
+            using var secureString = CreateSecureString(plainText);
+            if (plainText is null)
+            {
+                Assert.That(secureString, Is.Null);
+            }
+
+            Assert.That(() => secureString.IsNullOrEmpty(), Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("\u0020")]
+        [TestCase("\t")]
+        [TestCase("\r")]
+        [TestCase("\n")]
+        [TestCase(@"diNNEr/уЖин/晩ごはん/dÎner/晚餐")]
+        [SuppressMessage("ReSharper", "StringLiteralTypo")]
+        public void TestToPlainText(string? plainText)
+        {
+            using var secureString = CreateSecureString(plainText);
+            if (plainText is null)
+            {
+                Assert.That(secureString, Is.Null);
+            }
+
+            var actualResult = secureString.ToPlainText();
+            Assert.That(actualResult, Is.EqualTo(plainText));
+            if (plainText is null)
+            {
+                Assert.That(actualResult, Is.Null);
+            }
+        }
+
+        private static SecureString? CreateSecureString(string? plainText)
+        {
+            switch (plainText)
+            {
+                case null:
+                    return null;
+
+                default:
+                    var secureString = new SecureString();
+                    foreach (var ch in plainText!)
+                    {
+                        secureString.AppendChar(ch);
+                    }
+
+                    return secureString;
+            }
+        }
+    }
+}
---
+++ src/Omnifactotum/ExtensionMethods/OmnifactotumSecureStringExtensions.cs
@@ -0,0 +1,93 @@
+﻿#nullable enable
+
+#if (NETFRAMEWORK && !NET40) || NETSTANDARD || NETCOREAPP
+using System.Runtime.CompilerServices;
+#endif
+using System.Runtime.InteropServices;
+using System.Security;
+using Omnifactotum.Annotations;
+//// Use NETSTANDARD2_1_OR_GREATER once AppVeyor updates their VM image with VS2019 16.10 or higher
+#if NETSTANDARD2_1 || NET5_0_OR_GREATER
+using NotNullWhen = System.Diagnostics.CodeAnalysis.NotNullWhenAttribute;
+using NotNullIfNotNull = System.Diagnostics.CodeAnalysis.NotNullIfNotNullAttribute;
+#endif
+using PureAttribute = System.Diagnostics.Contracts.PureAttribute;
+
+//// ReSharper disable once CheckNamespace :: Namespace is intentionally named so in order to simplify usage of extension methods
+
+namespace System
+{
+    /// <summary>
+    ///     Contains extension methods for the <see cref="SecureString"/> class.
+    /// </summary>
+    public static class OmnifactotumSecureStringExtensions
+    {
+        /// <summary>
+        ///     Determines whether the specified <see cref="SecureString"/> is <see langword="null"/> or its
+        ///     <see cref="SecureString.Length"/> is zero.
+        /// </summary>
+        /// <param name="value">
+        ///     The <see cref="SecureString"/> value to test.
+        /// </param>
+        /// <returns>
+        ///     <see langword="true"/> if the specified <see cref="SecureString"/> is <see langword="null"/> or its
+        ///     <see cref="SecureString.Length"/> is zero; otherwise, <see langword="false"/>.
+        /// </returns>
+        [Pure]
+        [ContractAnnotation("false <= notnull", true)]
+#if (NETFRAMEWORK && !NET40) || NETSTANDARD || NETCOREAPP
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+        public static bool IsNullOrEmpty(
+#if NETSTANDARD2_1 || NET5_0_OR_GREATER
+            [NotNullWhen(false)]
+#endif
+            [CanBeNull] this SecureString? value)
+            => value is null || value.Length == 0;
+
+        /// <summary>
+        ///     Converts the specified <see cref="SecureString"/> value to a plain text <see cref="string"/>.
+        /// </summary>
+        /// <param name="value">
+        ///     The <see cref="SecureString"/> value to convert to a plain text <see cref="string"/>.
+        /// </param>
+        /// <returns>
+        ///     <see langword="null"/> if the specified <see cref="SecureString"/> value is <see langword="null"/>; otherwise, a new
+        ///     instance of <see cref="string"/> that contains the plain text from the specified <see cref="SecureString"/> value.
+        /// </returns>
+        [ContractAnnotation("null => null; notnull => notnull", true)]
+        [CanBeNull]
+        [Pure]
+#if NETSTANDARD2_1 || NET5_0_OR_GREATER
+        [return: NotNullIfNotNull(@"value")]
+#endif
+        public static string? ToPlainText([CanBeNull] this SecureString? value)
+        {
+            switch (value)
+            {
+                case null:
+                    return null;
+
+                case { Length: 0 }:
+                    return string.Empty;
+
+                default:
+                    {
+                        var intPtr = IntPtr.Zero;
+                        try
+                        {
+                            intPtr = Marshal.SecureStringToGlobalAllocUnicode(value);
+                            return Marshal.PtrToStringUni(intPtr) ?? string.Empty;
+                        }
+                        finally
+                        {
+                            if (intPtr != IntPtr.Zero)
+                            {
+                                Marshal.ZeroFreeGlobalAllocUnicode(intPtr);
+                            }
+                        }
+                    }
+            }
+        }
+    }
+}
