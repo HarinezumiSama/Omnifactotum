3ffd47abcbe1063ec8c1ffc5476bdf5c9d5fc844
Vitalii Maklai <vitalii.maklai@gmail.com>
2021-10-11 16:40:44 -0400

    Object validation: Added `WebUrlConstraint`

---
+++ src/Omnifactotum/Validation/Constraints/WebUrlConstraint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Omnifactotum.Validation.Constraints
+{
+    /// <summary>
+    ///     Specifies that the annotated member of the <see cref="String"/> type should be an absolute URI using
+    ///     the <see cref="Uri.UriSchemeHttp"/> or <see cref="Uri.UriSchemeHttps"/> scheme.
+    /// </summary>
+    public sealed class WebUrlConstraint : TypedMemberConstraintBase<string>
+    {
+        private static readonly HashSet<string> AllowedSchemes =
+            new(StringComparer.OrdinalIgnoreCase)
+            {
+                Uri.UriSchemeHttp,
+                Uri.UriSchemeHttps
+            };
+
+        /// <inheritdoc />
+        protected override void ValidateTypedValue(
+            ObjectValidatorContext validatorContext,
+            MemberConstraintValidationContext memberContext,
+            string value)
+        {
+            if (!Uri.TryCreate(value, UriKind.Absolute, out var uri) || !AllowedSchemes.Contains(uri.Scheme))
+            {
+                AddError(validatorContext, memberContext, $@"The value {value.ToUIString()} is not a valid Web URL.");
+            }
+        }
+    }
+}
