16766716b47f65b40d2cd180f7e1caa04be8daa8
Vitalii Maklai <vitalii.maklai@gmail.com>
2021-11-04 18:12:58 -0400

    Added `OmnifactotumEqualityComparerExtensions` (extension methods for `IEqualityComparer<T>`):
    - `GetHashCodeSafely(this IEqualityComparer<T>)`

---
+++ src/Omnifactotum.Tests/ExtensionMethods/OmnifactotumEqualityComparerExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+
+namespace Omnifactotum.Tests.ExtensionMethods
+{
+    [TestFixture(TestOf = typeof(OmnifactotumEqualityComparerExtensions))]
+    internal sealed class OmnifactotumEqualityComparerExtensionsTests
+    {
+        [Test]
+        [TestCase(null, int.MinValue)]
+        [TestCase(1, -1)]
+        [TestCase(-1, 1)]
+        [TestCase((int)sbyte.MaxValue, -sbyte.MaxValue)]
+        [TestCase(int.MaxValue, -int.MaxValue)]
+        public void TestGetHashCodeSafelyWithSpecifiedFallbackValue(int? value, int expectedResult)
+        {
+            var equalityComparerMock = new Mock<IEqualityComparer<int?>>(MockBehavior.Strict);
+
+            equalityComparerMock
+                .Setup(comparer => comparer.GetHashCode(It.IsAny<int?>()!))
+                .Returns<int?>(obj => -obj ?? throw new ArgumentNullException(nameof(obj)));
+
+            Assert.That(() => equalityComparerMock.Object.GetHashCodeSafely(value, int.MinValue), Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        [TestCase(null, 0)]
+        [TestCase(1, -1)]
+        [TestCase(-1, 1)]
+        [TestCase((int)sbyte.MaxValue, -sbyte.MaxValue)]
+        [TestCase(int.MaxValue, -int.MaxValue)]
+        public void TestGetHashCodeSafelyWithDefaultFallbackValue(int? value, int expectedResult)
+        {
+            var equalityComparerMock = new Mock<IEqualityComparer<int?>>(MockBehavior.Strict);
+
+            equalityComparerMock
+                .Setup(comparer => comparer.GetHashCode(It.IsAny<int?>()!))
+                .Returns<int?>(obj => -obj ?? throw new ArgumentNullException(nameof(obj)));
+
+            Assert.That(() => equalityComparerMock.Object.GetHashCodeSafely(value), Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ src/Omnifactotum/ExtensionMethods/OmnifactotumEqualityComparerExtensions.cs
@@ -0,0 +1,54 @@
+﻿#nullable enable
+
+using System.Diagnostics.CodeAnalysis;
+using NotNullAttribute = Omnifactotum.Annotations.NotNullAttribute;
+
+#if (NETFRAMEWORK && !NET40) || NETSTANDARD || NETCOREAPP
+using System.Runtime.CompilerServices;
+#endif
+
+//// ReSharper disable once CheckNamespace :: Namespace is intentionally named so in order to simplify usage of extension methods
+namespace System.Collections.Generic
+{
+    /// <summary>
+    ///     Contains extension methods for the <see cref="IEqualityComparer{T}"/> interface.
+    /// </summary>
+    [SuppressMessage("ReSharper", "RedundantNullnessAttributeWithNullableReferenceTypes", Justification = "Multiple target frameworks.")]
+    public static class OmnifactotumEqualityComparerExtensions
+    {
+        /// <summary>
+        ///     Gets a hash code of the specified value safely, that is, a <see langword="null"/> value does not cause an exception.
+        /// </summary>
+        /// <typeparam name="T">
+        ///     The type of the value to get a hash code of.
+        /// </typeparam>
+        /// <param name="equalityComparer">
+        ///     The equality comparer to use for getting a hash code of the value.
+        /// </param>
+        /// <param name="value">
+        ///     The value to get a hash code of. Can be <see langword="null"/>.
+        /// </param>
+        /// <param name="nullValueHashCode">
+        ///     The value to return if the <paramref name="value"/> parameter is <see langword="null"/>.
+        /// </param>
+        /// <returns>
+        ///     A hash code of the specified value obtained by calling <see cref="IEqualityComparer{T}.GetHashCode(T)"/> for this value
+        ///     if it is not <see langword="null"/>; otherwise, the value specified by the <paramref name="nullValueHashCode"/>
+        ///     parameter.
+        /// </returns>
+#if (NETFRAMEWORK && !NET40) || NETSTANDARD || NETCOREAPP
+        [MethodImpl(
+            MethodImplOptions.AggressiveInlining
+#if NET5_0_OR_GREATER
+            | MethodImplOptions.AggressiveOptimization
+#endif
+        )]
+#endif
+        public static int GetHashCodeSafely<T>([NotNull] this IEqualityComparer<T> equalityComparer, T value, int nullValueHashCode = 0)
+            => equalityComparer is null
+                ? throw new ArgumentNullException(nameof(equalityComparer))
+                : value is null
+                    ? nullValueHashCode
+                    : equalityComparer.GetHashCode(value);
+    }
+}
