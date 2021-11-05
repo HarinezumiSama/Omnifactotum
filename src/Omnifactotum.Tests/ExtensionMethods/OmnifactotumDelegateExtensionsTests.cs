660ae665df40043e6db92c1fdbedef78dcc82c6a
Vitalii Maklai <vitalii.maklai@gmail.com>
2021-11-05 10:32:48 -0400

    Added `OmnifactotumDelegateExtensions` (extension methods for delegates):
    - `GetTypedInvocations()`

---
+++ src/Omnifactotum.Tests/ExtensionMethods/OmnifactotumDelegateExtensionsTests.cs
@@ -0,0 +1,59 @@
+﻿#nullable enable
+
+using System;
+using NUnit.Framework;
+
+namespace Omnifactotum.Tests.ExtensionMethods
+{
+    [TestFixture(TestOf = typeof(OmnifactotumDelegateExtensions))]
+    internal sealed class OmnifactotumDelegateExtensionsTests
+    {
+        [Test]
+        public void TestGetTypedInvocations()
+        {
+            var obj = new TestClass();
+
+            Assert.That(() => obj.GetSomeActionInvocations(), Is.Empty);
+
+            obj.SomeAction += Handler1;
+
+            Assert.That(
+                () => obj.GetSomeActionInvocations(),
+                Is.EqualTo(new EventHandler<EventArgs>[] { Handler1 }));
+
+            obj.SomeAction += Handler2;
+
+            Assert.That(
+                () => obj.GetSomeActionInvocations(),
+                Is.EqualTo(new EventHandler<EventArgs>[] { Handler1, Handler2 }));
+
+            obj.SomeAction += Handler3;
+            obj.SomeAction += Handler1;
+            obj.SomeAction += Handler1;
+            obj.SomeAction += Handler2;
+
+            Assert.That(
+                () => obj.GetSomeActionInvocations(),
+                Is.EqualTo(new EventHandler<EventArgs>[] { Handler1, Handler2, Handler3, Handler1, Handler1, Handler2 }));
+
+            obj.SomeAction -= Handler1;
+
+            Assert.That(
+                () => obj.GetSomeActionInvocations(),
+                Is.EqualTo(new EventHandler<EventArgs>[] { Handler1, Handler2, Handler3, Handler1, Handler2 }));
+
+            static void Handler1(object? sender, EventArgs eventArgs) => throw new NotSupportedException();
+
+            static void Handler2(object? sender, EventArgs eventArgs) => throw new NotSupportedException();
+
+            static void Handler3(object? sender, EventArgs eventArgs) => throw new NotSupportedException();
+        }
+
+        private sealed class TestClass
+        {
+            public event EventHandler<EventArgs>? SomeAction;
+
+            public EventHandler<EventArgs>[] GetSomeActionInvocations() => SomeAction.GetTypedInvocations();
+        }
+    }
+}
---
+++ src/Omnifactotum/ExtensionMethods/OmnifactotumDelegateExtensions.cs
@@ -0,0 +1,69 @@
+﻿#nullable enable
+
+using System.Linq;
+using Omnifactotum.Annotations;
+
+#if (NETFRAMEWORK && !NET40) || NETSTANDARD || NETCOREAPP
+using System.Runtime.CompilerServices;
+#endif
+
+//// ReSharper disable RedundantNullnessAttributeWithNullableReferenceTypes
+
+//// ReSharper disable once CheckNamespace :: Namespace is intentionally named so in order to simplify usage of extension methods
+namespace System
+{
+    /// <summary>
+    ///     Contains extension methods for the <see cref="Delegate"/> types.
+    /// </summary>
+    public static class OmnifactotumDelegateExtensions
+    {
+        /// <summary>
+        ///     Gets the strongly typed invocation list of the specified delegate.
+        /// </summary>
+        /// <param name="delegate">
+        ///     The delegate to get the strongly typed invocation list of.
+        /// </param>
+        /// <typeparam name="TDelegate">
+        ///     The type of the delegate to get the invocations of.
+        /// </typeparam>
+        /// <returns>
+        ///     The strongly typed invocation list of the specified delegate.
+        /// </returns>
+        /// <seealso cref="Delegate.GetInvocationList"/>
+        /// <example>
+        ///     <code>
+        /// <![CDATA[
+        /// public event EventHandler<EventArgs>? SomeAction;
+        /// // ...
+        /// protected void OnSomeAction()
+        /// {
+        ///     var invocations = SomeAction.GetTypedInvocations();
+        ///     foreach (var invocation in invocations)
+        ///     {
+        ///         try
+        ///         {
+        ///             invocation(this, EventArgs.Empty);
+        ///         }
+        ///         catch (Exception)
+        ///         {
+        ///             //  Logging the exception
+        ///         }
+        ///     }
+        /// }
+        /// ]]>
+        ///     </code>
+        /// </example>
+#if (NETFRAMEWORK && !NET40) || NETSTANDARD || NETCOREAPP
+        [MethodImpl(
+            MethodImplOptions.AggressiveInlining
+#if NET5_0_OR_GREATER
+            | MethodImplOptions.AggressiveOptimization
+#endif
+        )]
+#endif
+        [NotNull]
+        public static TDelegate[] GetTypedInvocations<TDelegate>([CanBeNull] this TDelegate? @delegate)
+            where TDelegate : Delegate
+            => (@delegate?.GetInvocationList().Cast<TDelegate>().ToArray()).AvoidNull();
+    }
+}
