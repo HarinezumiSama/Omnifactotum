d4762230b92b6518f2500184fee5a28814985707
Vitalii Maklai <vitalii.maklai@gmail.com>
2022-09-14 14:18:56 -0400

    Validation: Added `NullableEnumValueDefinedConstraint<TEnum>` constraint

---
+++ src/Omnifactotum.Tests/Validation/Constraints/NullableEnumValueDefinedConstraintTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Omnifactotum.Validation.Constraints;
+
+namespace Omnifactotum.Tests.Validation.Constraints;
+
+[TestFixture(TestOf = typeof(NullableEnumValueDefinedConstraint<>))]
+internal sealed class NullableEnumValueDefinedConstraintTests : TypedConstraintTestsBase<NullableEnumValueDefinedConstraint<ConsoleColor>, ConsoleColor?>
+{
+    protected override IEnumerable<ConsoleColor?> GetTypedValidValues() => EnumFactotum.GetAllValues<ConsoleColor>().Select(item => (ConsoleColor?)item);
+
+    protected override IEnumerable<ConsoleColor?> GetTypedInvalidValues()
+    {
+        yield return (ConsoleColor)Enum.ToObject(typeof(ConsoleColor), -1);
+        yield return (ConsoleColor)Enum.ToObject(typeof(ConsoleColor), int.MinValue);
+        yield return (ConsoleColor)Enum.ToObject(typeof(ConsoleColor), int.MaxValue);
+    }
+}
---
+++ src/Omnifactotum/Validation/Constraints/NullableEnumValueDefinedConstraint.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static Omnifactotum.FormattableStringFactotum;
+
+namespace Omnifactotum.Validation.Constraints;
+
+/// <summary>
+///     Specifies that the annotated member of a nullable enumeration type should have a value defined in this nullable enumeration type.
+/// </summary>
+/// <typeparam name="TEnum">
+///     The type of the enumeration value to validate.
+/// </typeparam>
+public sealed class NullableEnumValueDefinedConstraint<TEnum> : TypedMemberConstraintBase<TEnum?>
+    where TEnum : struct, Enum
+{
+    /// <inheritdoc />
+    protected override void ValidateTypedValue(ObjectValidatorContext validatorContext, MemberConstraintValidationContext memberContext, TEnum? value)
+    {
+        if (value is null)
+        {
+            AddError(validatorContext, memberContext, ValidationMessages.CannotBeNull);
+            return;
+        }
+
+        if (!value.Value.IsDefined())
+        {
+            AddError(
+                validatorContext,
+                memberContext,
+                AsInvariant($@"The value {value:D} is not defined in the enumeration {typeof(TEnum).GetFullName().ToUIString()}."));
+        }
+    }
+}
