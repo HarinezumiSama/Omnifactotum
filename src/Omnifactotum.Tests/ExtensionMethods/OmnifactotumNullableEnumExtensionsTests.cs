fb38e8f1b99826d8353c10749fef254ef3ffd1b6
Vitalii Maklai <vitalii.maklai@gmail.com>
2022-09-14 00:10:40 -0400

    Added extension methods for nullable enumeration values `OmnifactotumNullableEnumExtensions`:
    - `CreateEnumValueNotImplementedException<TEnum>()`
    - `CreateEnumValueNotSupportedException<TEnum>()`

---
+++ src/Omnifactotum.Tests/ExtensionMethods/OmnifactotumNullableEnumExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Omnifactotum.Tests.ExtensionMethods;
+
+[TestFixture(TestOf = typeof(OmnifactotumNullableEnumExtensions))]
+internal sealed class OmnifactotumNullableEnumExtensionsTests
+{
+    [Test]
+    [TestCase(null, @"The operation for the null value (type: ConsoleColor?) is not implemented.")]
+    [TestCase(ConsoleColor.Magenta, @"The operation for the enumeration value ""ConsoleColor.Magenta"" is not implemented.")]
+    public void TestCreateEnumValueNotImplementedExceptionWhenValidArgumentIsPassedThenSucceeds(ConsoleColor? value, string expectedExceptionMessage)
+        => Assert.That(
+            () => value.CreateEnumValueNotImplementedException(),
+            Is.TypeOf<NotImplementedException>().With.Message.EqualTo(expectedExceptionMessage));
+
+    [Test]
+    [TestCase(null, @"The operation for the null value (type: ConsoleColor?) is not supported.")]
+    [TestCase(ConsoleColor.Yellow, @"The operation for the enumeration value ""ConsoleColor.Yellow"" is not supported.")]
+    public void TestCreateEnumValueNotSupportedExceptionWhenValidArgumentIsPassedThenSucceeds(ConsoleColor? value, string expectedExceptionMessage)
+        => Assert.That(
+            () => value.CreateEnumValueNotSupportedException(),
+            Is.TypeOf<NotSupportedException>().With.Message.EqualTo(expectedExceptionMessage));
+}
---
+++ src/Omnifactotum/ExtensionMethods/OmnifactotumNullableEnumExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.CompilerServices;
+using Omnifactotum;
+using static Omnifactotum.FormattableStringFactotum;
+using PureAttribute = System.Diagnostics.Contracts.PureAttribute;
+
+//// ReSharper disable RedundantNullnessAttributeWithNullableReferenceTypes
+//// ReSharper disable UseNullableReferenceTypesAnnotationSyntax
+
+//// ReSharper disable once CheckNamespace :: Namespace is intentionally named so in order to simplify usage of extension methods
+namespace System;
+
+/// <summary>
+///     Contains extension methods for nullable enumerations.
+/// </summary>
+public static class OmnifactotumNullableEnumExtensions
+{
+    /// <summary>
+    ///     Creates a <see cref="System.NotImplementedException"/> with the descriptive message regarding
+    ///     the specified enumeration value.
+    /// </summary>
+    /// <param name="enumerationValue">
+    ///     The enumeration value to create an exception for.
+    /// </param>
+    /// <returns>
+    ///     A <see cref="System.NotImplementedException"/> with the descriptive message regarding
+    ///     the specified enumeration value.
+    /// </returns>
+    [Pure]
+    [Omnifactotum.Annotations.Pure]
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Standard)]
+    public static NotImplementedException CreateEnumValueNotImplementedException<TEnum>(this TEnum? enumerationValue)
+        where TEnum : struct, Enum
+        => new(AsInvariant($@"The operation for {enumerationValue.InternalGetDescription()} is not implemented."));
+
+    /// <summary>
+    ///     Creates a <see cref="System.NotSupportedException"/> with the descriptive message regarding
+    ///     the specified enumeration value.
+    /// </summary>
+    /// <param name="enumerationValue">
+    ///     The enumeration value to create an exception for.
+    /// </param>
+    /// <returns>
+    ///     A <see cref="System.NotSupportedException"/> with the descriptive message regarding
+    ///     the specified enumeration value.
+    /// </returns>
+    [Pure]
+    [Omnifactotum.Annotations.Pure]
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Standard)]
+    public static NotSupportedException CreateEnumValueNotSupportedException<TEnum>(this TEnum? enumerationValue)
+        where TEnum : struct, Enum
+        => new(AsInvariant($@"The operation for {enumerationValue.InternalGetDescription()} is not supported."));
+
+    [Pure]
+    [Omnifactotum.Annotations.Pure]
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Standard)]
+    private static string InternalGetDescription<TEnum>(this TEnum? value)
+        where TEnum : struct, Enum
+        => value is { } enumerationValue
+            ? $@"the enumeration value {enumerationValue.GetQualifiedName().ToUIString()}"
+            : $@"the {OmnifactotumRepresentationConstants.NullValueRepresentation} value (type: {typeof(TEnum?).GetQualifiedName()})";
+}
