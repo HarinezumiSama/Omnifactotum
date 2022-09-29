b8973bd4dd10367012b699c06ca66474df2b8613
Vitalii Maklai <vitalii.maklai@gmail.com>
2022-09-29 14:35:11 -0400

    Added the `CaseInsensitiveStringKey` structure

---
+++ src/Omnifactotum.Tests/CaseInsensitiveStringKeyTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using Omnifactotum.NUnit;
+
+namespace Omnifactotum.Tests;
+
+[TestFixture(TestOf = typeof(CaseInsensitiveStringKey))]
+internal sealed class CaseInsensitiveStringKeyTests
+{
+    [Test]
+    [TestCase(null)]
+    [TestCase("")]
+    [TestCase("qWerTy")]
+    [TestCase("a53d7ef8a3ec403296F170342a9f70bf")]
+    public void TestConstruction(string? value)
+    {
+        var testee = new CaseInsensitiveStringKey(value);
+        Assert.That(() => testee.Value, Is.EqualTo(value));
+    }
+
+    [Test]
+    public void TestPropertyAccess() => NUnitFactotum.For<CaseInsensitiveStringKey>.AssertReadableWritable(obj => obj.Value, PropertyAccessMode.ReadOnly);
+
+    [Test]
+    [TestCase(null, null, AssertEqualityExpectation.EqualAndCannotBeSame)]
+    [TestCase(null, "", AssertEqualityExpectation.NotEqual)]
+    [TestCase(null, "\x0020", AssertEqualityExpectation.NotEqual)]
+    [TestCase(null, "2076092343F146AB98988C33745e2eb8", AssertEqualityExpectation.NotEqual)]
+    [TestCase("", "\x0020", AssertEqualityExpectation.NotEqual)]
+    [TestCase("\t", "\x0020", AssertEqualityExpectation.NotEqual)]
+    [TestCase("", "2076092343F146AB98988C33745e2eb8", AssertEqualityExpectation.NotEqual)]
+    [TestCase("", "", AssertEqualityExpectation.EqualAndCannotBeSame)]
+    [TestCase("Hello-World", "Hello-World", AssertEqualityExpectation.EqualAndCannotBeSame)]
+    [TestCase("Hello-World", "hello-world", AssertEqualityExpectation.EqualAndCannotBeSame)]
+    public void TestEquality(string? value1, string? value2, AssertEqualityExpectation equalityExpectation)
+    {
+        var testee1 = new CaseInsensitiveStringKey(value1);
+        var testee2 = new CaseInsensitiveStringKey(value2);
+        NUnitFactotum.AssertEquality(testee1, testee2, equalityExpectation, AssertEqualityOperatorExpectation.MustDefine);
+    }
+
+    [Test]
+    [TestCase(null, @"CaseInsensitiveStringKey { Value = null }")]
+    [TestCase("", @"CaseInsensitiveStringKey { Value = """" }")]
+    [TestCase("Hello-World", @"CaseInsensitiveStringKey { Value = ""Hello-World"" }")]
+    [TestCase("fbb097b992d144E1AC898e18a7d1ba39", @"CaseInsensitiveStringKey { Value = ""fbb097b992d144E1AC898e18a7d1ba39"" }")]
+    public void TestToString(string? value, string expectedResult)
+    {
+        var testee = new CaseInsensitiveStringKey(value);
+        Assert.That(() => testee.ToString(), Is.EqualTo(expectedResult));
+    }
+}
---
+++ src/Omnifactotum/CaseInsensitiveStringKey.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Omnifactotum.Annotations;
+
+namespace Omnifactotum;
+
+/// <summary>
+///     Represents the case-insensitive string key.
+/// </summary>
+public readonly struct CaseInsensitiveStringKey : IEquatable<CaseInsensitiveStringKey>
+{
+    /// <summary>
+    ///     <see cref="StringComparer"/> used to compare the underlying string value.
+    /// </summary>
+    [PublicAPI]
+    public static readonly StringComparer Comparer = StringComparer.OrdinalIgnoreCase;
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="CaseInsensitiveStringKey"/> structure.
+    /// </summary>
+    /// <param name="value">
+    ///     The original string value to create a <see cref="CaseInsensitiveStringKey"/> for.
+    /// </param>
+    public CaseInsensitiveStringKey(string? value) => Value = value;
+
+    /// <summary>
+    ///     Gets the original string value.
+    /// </summary>
+    public string? Value { get; }
+
+    /// <summary>
+    ///     Determines whether the two specified <see cref="CaseInsensitiveStringKey"/> instances are equal.
+    /// </summary>
+    /// <param name="left">
+    ///     The first <see cref="CaseInsensitiveStringKey"/> instance to compare.
+    /// </param>
+    /// <param name="right">
+    ///     The second <see cref="CaseInsensitiveStringKey"/> instance to compare.
+    /// </param>
+    /// <returns>
+    ///     <see langword="true"/> if the two specified <see cref="CaseInsensitiveStringKey"/> instances are equal; otherwise, <see langword="false"/>.
+    /// </returns>
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Maximum)]
+    public static bool operator ==(CaseInsensitiveStringKey left, CaseInsensitiveStringKey right) => Equals(left, right);
+
+    /// <summary>
+    ///     Determines whether the two specified <see cref="CaseInsensitiveStringKey"/> instances are not equal.
+    /// </summary>
+    /// <param name="left">
+    ///     The first <see cref="CaseInsensitiveStringKey"/> instance to compare.
+    /// </param>
+    /// <param name="right">
+    ///     The second <see cref="CaseInsensitiveStringKey"/> instance to compare.
+    /// </param>
+    /// <returns>
+    ///     <see langword="true"/> if the two specified <see cref="CaseInsensitiveStringKey"/> instances are not equal; otherwise, <see langword="false"/>.
+    /// </returns>
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Maximum)]
+    public static bool operator !=(CaseInsensitiveStringKey left, CaseInsensitiveStringKey right) => !Equals(left, right);
+
+    /// <inheritdoc />
+    public override bool Equals(object? obj) => obj is CaseInsensitiveStringKey castObj && Equals(castObj);
+
+    /// <inheritdoc />
+    public override int GetHashCode() => Value is null ? 0 : Comparer.GetHashCode(Value);
+
+    /// <inheritdoc />
+    public bool Equals(CaseInsensitiveStringKey other) => Equals(this, other);
+
+    /// <inheritdoc />
+    public override string ToString() => $@"{nameof(CaseInsensitiveStringKey)} {{ {nameof(Value)} = {Value.ToUIString()} }}";
+
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Maximum)]
+    private static bool Equals(CaseInsensitiveStringKey left, CaseInsensitiveStringKey right) => Comparer.Equals(left.Value, right.Value);
+}
