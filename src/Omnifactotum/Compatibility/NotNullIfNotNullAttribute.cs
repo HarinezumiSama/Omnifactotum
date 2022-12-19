e0aeb0df82cb48846a890cabb0d8f93d7fec0083
Vitalii Maklai <vitalii.maklai@gmail.com>
2022-12-19 18:01:25 -0500

    Fixed URL to "System/Diagnostics/CodeAnalysis/NullableAttributes.cs" in Compatibility attributes

--- src/Omnifactotum/Compatibility/NotNullIfNotNullAttribute.cs
+++ src/Omnifactotum/Compatibility/NotNullIfNotNullAttribute.cs
@@ -1,4 +1,4 @@
-﻿// Source: https://github.com/dotnet/runtime/blob/release/5.0/src/libraries/System.Private.CoreLib/src/System/Diagnostics/CodeAnalysis/NullableAttributes.cs
+﻿// Source: https://github.com/dotnet/runtime/blob/v5.0.0/src/libraries/System.Private.CoreLib/src/System/Diagnostics/CodeAnalysis/NullableAttributes.cs
 
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
