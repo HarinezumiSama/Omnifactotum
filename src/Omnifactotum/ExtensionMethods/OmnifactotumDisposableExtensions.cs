77a4d13fd666a22092a9fd0c95d14c357bd78b23
Vitalii Maklai <vitalii.maklai@gmail.com>
2022-12-27 01:51:08 -0500

    OmnifactotumDisposableExtensions (.NET Standard 2.1+ and .NET 5+): Added `ConfigureAwaitNoCapturedContext(this IAsyncDisposable)`

--- src/Omnifactotum/ExtensionMethods/OmnifactotumDisposableExtensions.cs
+++ src/Omnifactotum/ExtensionMethods/OmnifactotumDisposableExtensions.cs
@@ -2,15 +2,25 @@
 using Omnifactotum;
 using Omnifactotum.Annotations;
 
+#if NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER
+using System.Threading.Tasks;
+#endif
+
 //// ReSharper disable RedundantNullnessAttributeWithNullableReferenceTypes
 //// ReSharper disable UseNullableReferenceTypesAnnotationSyntax
 
 //// ReSharper disable once CheckNamespace :: Namespace is intentionally named so in order to simplify usage of extension methods
 namespace System;
 
+#if NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER
 /// <summary>
-///     Contains extension methods for <see cref="IDisposable"/> interface.
+///     Contains extension methods for the <see cref="IDisposable"/> and <see cref="IAsyncDisposable"/> interfaces.
 /// </summary>
+#else
+/// <summary>
+///     Contains extension methods for the <see cref="IDisposable"/> interface.
+/// </summary>
+#endif
 public static class OmnifactotumDisposableExtensions
 {
     /// <summary>
@@ -44,4 +54,23 @@ public static class OmnifactotumDisposableExtensions
     public static void DisposeSafely<T>([CanBeNull] this T? disposable)
         where T : struct, IDisposable
         => disposable?.Dispose();
+
+#if NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER
+    /// <summary>
+    ///     Configures an async disposable so that the awaits on the tasks returned from this disposable do not attempt
+    ///     to marshal the continuation back to the original context captured.
+    /// </summary>
+    /// <param name="source">
+    ///     The source async disposable.
+    /// </param>
+    /// <returns>
+    ///     The configured async disposable.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    ///     <paramref name="source"/> is <see langword="null"/>.
+    /// </exception>
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Maximum)]
+    public static ConfiguredAsyncDisposable ConfigureAwaitNoCapturedContext([NotNull] this IAsyncDisposable source)
+        => (source ?? throw new ArgumentNullException(nameof(source))).ConfigureAwait(false);
+#endif
 }
