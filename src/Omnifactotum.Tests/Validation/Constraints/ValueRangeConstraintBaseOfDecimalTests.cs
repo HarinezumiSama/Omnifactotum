7f998ac2ce08a54e99b9293fd786b584913e27fa
Vitalii Maklai <vitalii.maklai@gmail.com>
2023-05-23 16:38:47 -0400

    Object validation: Added `ValueRangeConstraintBase<T>`

---
+++ src/Omnifactotum.Tests/Validation/Constraints/ValueRangeConstraintBaseOfDecimalTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Omnifactotum.Validation.Constraints;
+
+namespace Omnifactotum.Tests.Validation.Constraints;
+
+[TestFixture(TestOf = typeof(ValueRangeConstraintBase<>))]
+internal sealed class ValueRangeConstraintBaseOfDecimalTests
+    : TypedConstraintTestsBase<ValueRangeConstraintBaseOfDecimalTests.ValueRangeConstraint, decimal>
+{
+    protected override IEnumerable<decimal> GetTypedValidValues()
+    {
+        yield return 1.1m;
+        yield return 1.234m;
+        yield return 2.345m;
+        yield return 3.45678m;
+        yield return 3.9m;
+    }
+
+    protected override IEnumerable<decimal> GetTypedInvalidValues()
+    {
+        yield return decimal.MinValue;
+        yield return 0m;
+        yield return 1m;
+        yield return 1.0999m;
+        yield return 3.900001m;
+        yield return decimal.MaxValue;
+    }
+
+    protected override string GetTypedInvalidValueErrorMessage(decimal invalidValue) => "The value must be within the range [ 1.1 ~ 3.9 ].";
+
+    internal sealed class ValueRangeConstraint : ValueRangeConstraintBase<decimal>
+    {
+        public ValueRangeConstraint()
+            : base(ValueRange.Create(1.1m, 3.9m))
+        {
+            // Nothing to do
+        }
+    }
+}
---
+++ src/Omnifactotum/Validation/Constraints/ValueRangeConstraintBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using static Omnifactotum.FormattableStringFactotum;
+
+namespace Omnifactotum.Validation.Constraints;
+
+/// <summary>
+///     Represents a base constraint specifying that the annotated member of the type <typeparamref name="T"/> should be within the specified range.
+/// </summary>
+public abstract class ValueRangeConstraintBase<T> : TypedMemberConstraintBase<T>
+    where T : IComparable
+{
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="ValueRangeConstraintBase{T}" /> class.
+    /// </summary>
+    /// <param name="range">
+    ///     The range of valid values.
+    /// </param>
+    protected ValueRangeConstraintBase(ValueRange<T> range) => Range = range;
+
+    /// <summary>
+    ///     Gets the range of valid values.
+    /// </summary>
+    protected ValueRange<T> Range { get; }
+
+    /// <summary>
+    ///     Formats <see cref="Range"/> as a string to use in the validation error message as needed.
+    /// </summary>
+    /// <returns>
+    ///     The formatted <see cref="Range"/>.
+    /// </returns>
+    protected virtual string FormatRange() => AsInvariant($"[ {Range.Lower} ~ {Range.Upper} ]");
+
+    /// <inheritdoc />
+    protected sealed override void ValidateTypedValue(
+        ObjectValidatorContext validatorContext,
+        MemberConstraintValidationContext memberContext,
+        T value)
+    {
+        if (Range.Contains(value))
+        {
+            return;
+        }
+
+        AddError(validatorContext, memberContext, $"The value must be within the range {FormatRange()}.");
+    }
+}
