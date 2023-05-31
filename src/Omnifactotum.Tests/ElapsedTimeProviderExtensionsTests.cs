1f45b1662eedec23a89e29cb29c2d8adeeaa932d
Vitalii Maklai <vitalii.maklai@gmail.com>
2023-05-31 18:49:54 -0400

    ElapsedTimeProviderExtensions: Added the `GetStoppedElapsed()` extension method

---
+++ src/Omnifactotum.Tests/ElapsedTimeProviderExtensionsTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Moq;
+using NUnit.Framework;
+using Omnifactotum.Abstractions;
+
+namespace Omnifactotum.Tests;
+
+[TestFixture(TestOf = typeof(ElapsedTimeProviderExtensions))]
+internal sealed class ElapsedTimeProviderExtensionsTests
+{
+    private static readonly TimeSpan WaitInterval = TimeSpan.FromMilliseconds(10);
+
+    [Test]
+    public void TestRestart()
+    {
+        var elapsedTimeProviderMock = new Mock<IElapsedTimeProvider>(MockBehavior.Strict);
+
+        var calledMethods = new List<CalledMethod>();
+
+        elapsedTimeProviderMock
+            .Setup(provider => provider.Reset())
+            .Callback(() => calledMethods.Add(CalledMethod.Reset));
+
+        elapsedTimeProviderMock
+            .Setup(provider => provider.Start())
+            .Callback(() => calledMethods.Add(CalledMethod.Start));
+
+        elapsedTimeProviderMock.Object.Restart();
+
+        Assert.That(calledMethods, Is.EqualTo(new[] { CalledMethod.Reset, CalledMethod.Start }));
+        elapsedTimeProviderMock.Verify(provider => provider.Reset(), Times.Once);
+        elapsedTimeProviderMock.Verify(provider => provider.Start(), Times.Once);
+    }
+
+    [Test]
+    public void TestGetStoppedElapsedWhenInvalidArgumentThenThrows()
+        => Assert.That(() => default(IElapsedTimeProvider)!.GetStoppedElapsed(), Throws.ArgumentNullException);
+
+    [Test]
+    public void TestGetStoppedElapsedWhenValidArgumentThenSucceeds()
+    {
+        var stopwatchElapsedTimeProvider = new StopwatchElapsedTimeProvider();
+
+        Assert.That(() => stopwatchElapsedTimeProvider.IsRunning, Is.False);
+        Assert.That(() => stopwatchElapsedTimeProvider.Elapsed, Is.EqualTo(TimeSpan.Zero));
+        Assert.That(() => stopwatchElapsedTimeProvider.GetStoppedElapsed(), Is.EqualTo(TimeSpan.Zero));
+        Assert.That(() => stopwatchElapsedTimeProvider.IsRunning, Is.False);
+
+        Thread.Sleep(WaitInterval);
+
+        Assert.That(() => stopwatchElapsedTimeProvider.IsRunning, Is.False);
+        Assert.That(() => stopwatchElapsedTimeProvider.Elapsed, Is.EqualTo(TimeSpan.Zero));
+        Assert.That(() => stopwatchElapsedTimeProvider.GetStoppedElapsed(), Is.EqualTo(TimeSpan.Zero));
+        Assert.That(() => stopwatchElapsedTimeProvider.IsRunning, Is.False);
+
+        stopwatchElapsedTimeProvider.Start();
+        Assert.That(() => stopwatchElapsedTimeProvider.IsRunning, Is.True);
+
+        Thread.Sleep(WaitInterval);
+
+        var stoppedElapsed1 = stopwatchElapsedTimeProvider.GetStoppedElapsed();
+        Assert.That(() => stoppedElapsed1, Is.GreaterThanOrEqualTo(WaitInterval));
+
+        Assert.That(() => stopwatchElapsedTimeProvider.IsRunning, Is.False);
+        Assert.That(() => stopwatchElapsedTimeProvider.Elapsed, Is.EqualTo(stoppedElapsed1));
+        Assert.That(() => stopwatchElapsedTimeProvider.GetStoppedElapsed(), Is.EqualTo(stoppedElapsed1));
+        Assert.That(() => stopwatchElapsedTimeProvider.IsRunning, Is.False);
+        Assert.That(() => stopwatchElapsedTimeProvider.Elapsed, Is.EqualTo(stoppedElapsed1));
+
+        Thread.Sleep(WaitInterval);
+
+        Assert.That(() => stopwatchElapsedTimeProvider.IsRunning, Is.False);
+        Assert.That(() => stopwatchElapsedTimeProvider.Elapsed, Is.EqualTo(stoppedElapsed1));
+        Assert.That(() => stopwatchElapsedTimeProvider.GetStoppedElapsed(), Is.EqualTo(stoppedElapsed1));
+        Assert.That(() => stopwatchElapsedTimeProvider.IsRunning, Is.False);
+        Assert.That(() => stopwatchElapsedTimeProvider.Elapsed, Is.EqualTo(stoppedElapsed1));
+
+        stopwatchElapsedTimeProvider.Start();
+
+        Thread.Sleep(WaitInterval);
+
+        var stoppedElapsed2 = stopwatchElapsedTimeProvider.GetStoppedElapsed();
+        Assert.That(() => stoppedElapsed2, Is.GreaterThanOrEqualTo(WaitInterval));
+
+        Assert.That(() => stopwatchElapsedTimeProvider.IsRunning, Is.False);
+        Assert.That(() => stopwatchElapsedTimeProvider.Elapsed, Is.EqualTo(stoppedElapsed2));
+        Assert.That(() => stopwatchElapsedTimeProvider.GetStoppedElapsed(), Is.EqualTo(stoppedElapsed2));
+        Assert.That(() => stopwatchElapsedTimeProvider.IsRunning, Is.False);
+        Assert.That(() => stopwatchElapsedTimeProvider.Elapsed, Is.EqualTo(stoppedElapsed2));
+    }
+
+    private enum CalledMethod
+    {
+        Reset,
+        Start
+    }
+}
--- src/Omnifactotum/Abstractions/ElapsedTimeProviderExtensions.cs
+++ src/Omnifactotum/Abstractions/ElapsedTimeProviderExtensions.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+using Omnifactotum.Annotations;
+
+//// ReSharper disable RedundantNullnessAttributeWithNullableReferenceTypes
+//// ReSharper disable UseNullableReferenceTypesAnnotationSyntax
 
 namespace Omnifactotum.Abstractions;
 
@@ -19,5 +24,26 @@ public static class ElapsedTimeProviderExtensions
 
         elapsedTimeProvider.Reset();
         elapsedTimeProvider.Start();
+    }
+
+    /// <summary>
+    ///     Gets the total elapsed time measured by the specified <see cref="IElapsedTimeProvider"/> that was stopped before measuring.
+    /// </summary>
+    /// <param name="elapsedTimeProvider">
+    ///     <see cref="IElapsedTimeProvider"/> to stop and measure the total elapsed time of.
+    /// </param>
+    /// <returns>
+    ///     The total elapsed time measured by the specified <see cref="IElapsedTimeProvider"/> that was stopped before measuring.
+    /// </returns>
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Standard)]
+    public static TimeSpan GetStoppedElapsed([NotNull] this IElapsedTimeProvider elapsedTimeProvider)
+    {
+        if (elapsedTimeProvider is null)
+        {
+            throw new ArgumentNullException(nameof(elapsedTimeProvider));
+        }
+
+        elapsedTimeProvider.Stop();
+        return elapsedTimeProvider.Elapsed;
     }
 }
