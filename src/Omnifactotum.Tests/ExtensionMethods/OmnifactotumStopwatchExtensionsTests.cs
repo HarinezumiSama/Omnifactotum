fd179b9fd9703df85c057b74c5d2643754b4571d
Vitalii Maklai <vitalii.maklai@gmail.com>
2023-05-31 18:48:07 -0400

    Added `OmnifactotumStopwatchExtensions` with the `GetStoppedElapsed()` extension method

---
+++ src/Omnifactotum.Tests/ExtensionMethods/OmnifactotumStopwatchExtensionsTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Omnifactotum.Tests.ExtensionMethods;
+
+[TestFixture(TestOf = typeof(OmnifactotumStopwatchExtensions))]
+internal sealed class OmnifactotumStopwatchExtensionsTests
+{
+    private static readonly TimeSpan WaitInterval = TimeSpan.FromMilliseconds(10);
+
+    [Test]
+    public void TestGetStoppedElapsedWhenInvalidArgumentThenThrows()
+        => Assert.That(() => default(Stopwatch)!.GetStoppedElapsed(), Throws.ArgumentNullException);
+
+    [Test]
+    public void TestGetStoppedElapsedWhenValidArgumentThenSucceeds()
+    {
+        var stopwatch = new Stopwatch();
+
+        Assert.That(() => stopwatch.IsRunning, Is.False);
+        Assert.That(() => stopwatch.Elapsed, Is.EqualTo(TimeSpan.Zero));
+        Assert.That(() => stopwatch.GetStoppedElapsed(), Is.EqualTo(TimeSpan.Zero));
+        Assert.That(() => stopwatch.IsRunning, Is.False);
+
+        Thread.Sleep(WaitInterval);
+
+        Assert.That(() => stopwatch.IsRunning, Is.False);
+        Assert.That(() => stopwatch.Elapsed, Is.EqualTo(TimeSpan.Zero));
+        Assert.That(() => stopwatch.GetStoppedElapsed(), Is.EqualTo(TimeSpan.Zero));
+        Assert.That(() => stopwatch.IsRunning, Is.False);
+
+        stopwatch.Start();
+        Assert.That(() => stopwatch.IsRunning, Is.True);
+
+        Thread.Sleep(WaitInterval);
+
+        var stoppedElapsed1 = stopwatch.GetStoppedElapsed();
+        Assert.That(() => stoppedElapsed1, Is.GreaterThanOrEqualTo(WaitInterval));
+
+        Assert.That(() => stopwatch.IsRunning, Is.False);
+        Assert.That(() => stopwatch.Elapsed, Is.EqualTo(stoppedElapsed1));
+        Assert.That(() => stopwatch.GetStoppedElapsed(), Is.EqualTo(stoppedElapsed1));
+        Assert.That(() => stopwatch.IsRunning, Is.False);
+        Assert.That(() => stopwatch.Elapsed, Is.EqualTo(stoppedElapsed1));
+
+        Thread.Sleep(WaitInterval);
+
+        Assert.That(() => stopwatch.IsRunning, Is.False);
+        Assert.That(() => stopwatch.Elapsed, Is.EqualTo(stoppedElapsed1));
+        Assert.That(() => stopwatch.GetStoppedElapsed(), Is.EqualTo(stoppedElapsed1));
+        Assert.That(() => stopwatch.IsRunning, Is.False);
+        Assert.That(() => stopwatch.Elapsed, Is.EqualTo(stoppedElapsed1));
+
+        stopwatch.Start();
+
+        Thread.Sleep(WaitInterval);
+
+        var stoppedElapsed2 = stopwatch.GetStoppedElapsed();
+        Assert.That(() => stoppedElapsed2, Is.GreaterThanOrEqualTo(WaitInterval));
+
+        Assert.That(() => stopwatch.IsRunning, Is.False);
+        Assert.That(() => stopwatch.Elapsed, Is.EqualTo(stoppedElapsed2));
+        Assert.That(() => stopwatch.GetStoppedElapsed(), Is.EqualTo(stoppedElapsed2));
+        Assert.That(() => stopwatch.IsRunning, Is.False);
+        Assert.That(() => stopwatch.Elapsed, Is.EqualTo(stoppedElapsed2));
+    }
+}
---
+++ src/Omnifactotum/ExtensionMethods/OmnifactotumStopwatchExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+using Omnifactotum;
+using Omnifactotum.Annotations;
+
+//// ReSharper disable RedundantNullnessAttributeWithNullableReferenceTypes
+//// ReSharper disable UseNullableReferenceTypesAnnotationSyntax
+
+//// ReSharper disable once CheckNamespace :: Namespace is intentionally named so in order to simplify usage of extension methods
+namespace System.Diagnostics;
+
+/// <summary>
+///     Contains extension methods for the <see cref="Stopwatch"/> type.
+/// </summary>
+public static class OmnifactotumStopwatchExtensions
+{
+    /// <summary>
+    ///     Gets the total elapsed time measured by the specified <see cref="Stopwatch"/> that was stopped before measuring.
+    /// </summary>
+    /// <param name="stopwatch">
+    ///     <see cref="Stopwatch"/> to stop and measure the total elapsed time of.
+    /// </param>
+    /// <returns>
+    ///     The total elapsed time measured by the specified <see cref="Stopwatch"/> that was stopped before measuring.
+    /// </returns>
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Standard)]
+    public static TimeSpan GetStoppedElapsed([NotNull] this Stopwatch stopwatch)
+    {
+        if (stopwatch is null)
+        {
+            throw new ArgumentNullException(nameof(stopwatch));
+        }
+
+        stopwatch.Stop();
+        return stopwatch.Elapsed;
+    }
+}
