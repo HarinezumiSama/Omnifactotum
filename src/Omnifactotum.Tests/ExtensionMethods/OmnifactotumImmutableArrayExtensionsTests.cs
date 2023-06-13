d166b700d88bcca06de3f5d722e7caeb01e50a06
Vitalii Maklai <vitalii.maklai@gmail.com>
2023-06-13 12:09:21 -0400

    `OmnifactotumImmutableArrayExtensions`: Added the `AvoidNullOrDefault<T>(this ImmutableArray<T>?)` extension method
    Also, applied `Pure` annotations to `AvoidDefault<T>()` and `AvoidNullOrDefault<T>()`

--- src/Omnifactotum.Tests/ExtensionMethods/OmnifactotumImmutableArrayExtensionsTests.cs
+++ src/Omnifactotum.Tests/ExtensionMethods/OmnifactotumImmutableArrayExtensionsTests.cs
@@ -16,6 +16,9 @@ internal sealed class OmnifactotumImmutableArrayExtensionsTests
         ExecuteTestCase(default(ImmutableArray<int>), Array.Empty<int>());
         ExecuteTestCase(default(ImmutableArray<string>), Array.Empty<string>());
 
+        ExecuteTestCase(new ImmutableArray<int>(), Array.Empty<int>());
+        ExecuteTestCase(new ImmutableArray<string>(), Array.Empty<string>());
+
         ExecuteTestCase(ImmutableArray.Create(-13, 17), new[] { -13, 17 });
         ExecuteTestCase(ImmutableArray.Create("Hello", "people"), new[] { "Hello", "people" });
 
@@ -23,6 +26,33 @@ internal sealed class OmnifactotumImmutableArrayExtensionsTests
         {
             expectedResult.AssertNotNull();
             Assert.That(() => input.AvoidDefault(), Has.Property(nameof(ImmutableArray<object>.IsDefault)).False & Is.EqualTo(expectedResult));
+        }
+    }
+
+    [Test]
+    [SuppressMessage("ReSharper", "ArrangeDefaultValueWhenTypeNotEvident")]
+    [SuppressMessage("ReSharper", "RedundantCast")]
+    public void TestAvoidNullOrDefaultSucceeds()
+    {
+        ExecuteTestCase(default(ImmutableArray<int>?), Array.Empty<int>());
+        ExecuteTestCase(default(ImmutableArray<string>?), Array.Empty<string>());
+
+        ExecuteTestCase((ImmutableArray<int>?)null, Array.Empty<int>());
+        ExecuteTestCase((ImmutableArray<string>?)null, Array.Empty<string>());
+
+        ExecuteTestCase(default(ImmutableArray<int>), Array.Empty<int>());
+        ExecuteTestCase(default(ImmutableArray<string>), Array.Empty<string>());
+
+        ExecuteTestCase(new ImmutableArray<int>(), Array.Empty<int>());
+        ExecuteTestCase(new ImmutableArray<string>(), Array.Empty<string>());
+
+        ExecuteTestCase(ImmutableArray.Create(-13, 17), new[] { -13, 17 });
+        ExecuteTestCase(ImmutableArray.Create("Hello", "people"), new[] { "Hello", "people" });
+
+        static void ExecuteTestCase<T>(ImmutableArray<T>? input, T[] expectedResult)
+        {
+            expectedResult.AssertNotNull();
+            Assert.That(() => input.AvoidNullOrDefault(), Has.Property(nameof(ImmutableArray<object>.IsDefault)).False & Is.EqualTo(expectedResult));
         }
     }
 }
--- src/Omnifactotum/ExtensionMethods/OmnifactotumImmutableArrayExtensions.cs
+++ src/Omnifactotum/ExtensionMethods/OmnifactotumImmutableArrayExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
 using Omnifactotum;
 using Omnifactotum.Annotations;
+using PureAttribute = System.Diagnostics.Contracts.PureAttribute;
 
 //// ReSharper disable RedundantNullnessAttributeWithNullableReferenceTypes
 //// ReSharper disable UseNullableReferenceTypesAnnotationSyntax
@@ -29,7 +30,31 @@ public static class OmnifactotumImmutableArrayExtensions
     ///     otherwise, <see cref="ImmutableArray{T}.Empty"/>.
     /// </returns>
     /// <seealso cref="ImmutableArray{T}.IsDefault"/>
+    [Pure]
+    [Omnifactotum.Annotations.Pure]
     [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Maximum)]
     public static ImmutableArray<T> AvoidDefault<T>([NoEnumeration] this ImmutableArray<T> source)
         => source is { IsDefault: true } ? ImmutableArray<T>.Empty : source;
+
+    /// <summary>
+    ///     Avoids the specified nullable <see cref="ImmutableArray{T}"/> being a <see langword="null"/> reference or an uninitialized instance.
+    ///     Returns the specified <see cref="ImmutableArray{T}"/> if it is not a <see langword="null"/> reference and is initialized
+    ///     (that is, <see cref="ImmutableArray{T}.IsDefault"/> is <see langword="false"/>); otherwise, returns <see cref="ImmutableArray{T}.Empty"/>.
+    /// </summary>
+    /// <typeparam name="T">
+    ///     The type of elements in the immutable array.
+    /// </typeparam>
+    /// <param name="source">
+    ///     The collection to secure from a <see langword="null"/> reference or from being an uninitialized instance.
+    /// </param>
+    /// <returns>
+    ///     The source immutable array if it is not <see langword="null"/> and is initialized
+    ///     (that is, <see cref="ImmutableArray{T}.IsDefault"/> is <see langword="false"/>); otherwise, <see cref="ImmutableArray{T}.Empty"/>.
+    /// </returns>
+    /// <seealso cref="ImmutableArray{T}.IsDefault"/>
+    [Pure]
+    [Omnifactotum.Annotations.Pure]
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Maximum)]
+    public static ImmutableArray<T> AvoidNullOrDefault<T>([CanBeNull] [NoEnumeration] this ImmutableArray<T>? source)
+        => source is { IsDefault: false } array ? array : ImmutableArray<T>.Empty;
 }
