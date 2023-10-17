77c002d54d4c8ee6e3d6db2e8905afc1ca4adc87
Vitalii Maklai <vitalii.maklai@gmail.com>
2023-10-16 20:41:01 -0400

    Validation: Added `OptionalValueRangeConstraintBase<T>` constraint

---
+++ src/Omnifactotum.Tests/Validation/Constraints/OptionalValueRangeConstraintBaseOfDecimalTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Omnifactotum.NUnit;
+using Omnifactotum.Validation.Constraints;
+using static Omnifactotum.FormattableStringFactotum;
+
+namespace Omnifactotum.Tests.Validation.Constraints;
+
+[TestFixture(TestOf = typeof(OptionalValueRangeConstraintBase<>))]
+internal sealed class OptionalValueRangeConstraintBaseOfDecimalTests
+    : TypedConstraintTestsBase<OptionalValueRangeConstraintBaseOfDecimalTests.ValueRangeConstraint, decimal?>
+{
+    protected override void OnTestConstruction(ValueRangeConstraint testee)
+    {
+        base.OnTestConstruction(testee);
+
+        Assert.That(() => testee.ExposedRange.Lower, Is.EqualTo(1.1m));
+        Assert.That(() => testee.ExposedRange.Upper, Is.EqualTo(3.9m));
+    }
+
+    protected override IEnumerable<decimal?> GetTypedValidValues()
+    {
+        yield return null;
+        yield return 1.1m;
+        yield return 1.234m;
+        yield return 2.345m;
+        yield return 3.45678m;
+        yield return 3.9m;
+    }
+
+    protected override IEnumerable<decimal?> GetTypedInvalidValues()
+    {
+        yield return decimal.MinValue;
+        yield return 0m;
+        yield return 1m;
+        yield return 1.0999m;
+        yield return 3.900001m;
+        yield return decimal.MaxValue;
+    }
+
+    protected override string GetTypedInvalidValueErrorMessage(decimal? invalidValue)
+        => AsInvariant($"The value {invalidValue.AssertNotNull()} is not within the valid range [1.1 ~ 3.9].");
+
+    internal sealed class ValueRangeConstraint : OptionalValueRangeConstraintBase<decimal>
+    {
+        public ValueRangeConstraint()
+            : base(1.1m, 3.9m)
+        {
+            // Nothing to do
+        }
+
+        public ValueRange<decimal> ExposedRange => Range;
+    }
+}
---
+++ src/Omnifactotum.Tests/Validation/Constraints/OptionalValueRangeConstraintBaseOfInt32Tests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Omnifactotum.NUnit;
+using Omnifactotum.Validation.Constraints;
+using static Omnifactotum.FormattableStringFactotum;
+
+namespace Omnifactotum.Tests.Validation.Constraints;
+
+[TestFixture(TestOf = typeof(OptionalValueRangeConstraintBase<>))]
+internal sealed class OptionalValueRangeConstraintBaseOfInt32Tests
+    : TypedConstraintTestsBase<OptionalValueRangeConstraintBaseOfInt32Tests.ValueRangeConstraint, int?>
+{
+    protected override void OnTestConstruction(ValueRangeConstraint testee)
+    {
+        base.OnTestConstruction(testee);
+
+        Assert.That(() => testee.ExposedRange.Lower, Is.EqualTo(-17));
+        Assert.That(() => testee.ExposedRange.Upper, Is.EqualTo(23));
+    }
+
+    protected override IEnumerable<int?> GetTypedValidValues() => Enumerable.Range(-17, 23).Select(i => i.AsNullable()).Prepend(null);
+
+    protected override IEnumerable<int?> GetTypedInvalidValues()
+    {
+        yield return int.MinValue;
+        yield return -91;
+        yield return -18;
+        yield return 24;
+        yield return 37;
+        yield return int.MaxValue;
+    }
+
+    protected override string GetTypedInvalidValueErrorMessage(int? invalidValue)
+        => AsInvariant($"The value {invalidValue.AssertNotNull()} is not within the valid range ({{ <-17> .:. <23> }}).");
+
+    internal sealed class ValueRangeConstraint : OptionalValueRangeConstraintBase<int>
+    {
+        public ValueRangeConstraint()
+            : base(ValueRange.Create(-17, 23))
+        {
+            // Nothing to do
+        }
+
+        public ValueRange<int> ExposedRange => Range;
+
+        protected override string FormatRange() => AsInvariant($"({{ <{Range.Lower}> .:. <{Range.Upper}> }})");
+    }
+}
