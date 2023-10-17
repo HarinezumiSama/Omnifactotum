8989a97059366366c041938b96afe569af7fc5d5
Vitalii Maklai <vitalii.maklai@gmail.com>
2023-10-16 20:49:51 -0400

    Validation: Added `OptionalEnumValueDefinedConstraint<T>` constraint

---
+++ src/Omnifactotum.Tests/Validation/Constraints/OptionalEnumValueDefinedConstraintTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Omnifactotum.NUnit;
+using Omnifactotum.Validation.Constraints;
+using static Omnifactotum.FormattableStringFactotum;
+
+namespace Omnifactotum.Tests.Validation.Constraints;
+
+[TestFixture(TestOf = typeof(OptionalEnumValueDefinedConstraint<>))]
+internal sealed class OptionalEnumValueDefinedConstraintTests : TypedConstraintTestsBase<OptionalEnumValueDefinedConstraint<ConsoleColor>, ConsoleColor?>
+{
+    protected override IEnumerable<ConsoleColor?> GetTypedValidValues()
+        => EnumFactotum.GetAllValues<ConsoleColor>().Select(item => (ConsoleColor?)item).Prepend(null);
+
+    protected override IEnumerable<ConsoleColor?> GetTypedInvalidValues()
+    {
+        yield return (ConsoleColor)(-1);
+        yield return (ConsoleColor)int.MinValue;
+        yield return (ConsoleColor)int.MaxValue;
+    }
+
+    protected override string GetTypedInvalidValueErrorMessage(ConsoleColor? invalidValue)
+        => AsInvariant($@"The value {(int)invalidValue.AssertNotNull()} is not defined in the enumeration ""System.ConsoleColor"".");
+}
---
+++ src/Omnifactotum/Validation/Constraints/OptionalEnumValueDefinedConstraint.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static Omnifactotum.FormattableStringFactotum;
+
+namespace Omnifactotum.Validation.Constraints;
+
+/// <summary>
+///     Represents a base constraint specifying that the annotated member of the nullable enumeration type <typeparamref name="TEnum"/>
+///     should be either <see langword="null"/> or a value defined in the enumeration type <typeparamref name="TEnum"/>.
+/// </summary>
+/// <typeparam name="TEnum">
+///     The type of the enumeration value to validate.
+/// </typeparam>
+public sealed class OptionalEnumValueDefinedConstraint<TEnum> : TypedMemberConstraintBase<TEnum?>
+    where TEnum : struct, Enum
+{
+    /// <inheritdoc />
+    protected override void ValidateTypedValue(ObjectValidatorContext validatorContext, MemberConstraintValidationContext memberContext, TEnum? value)
+    {
+        if (value is null)
+        {
+            return;
+        }
+
+        if (!value.Value.IsDefined())
+        {
+            AddError(
+                validatorContext,
+                memberContext,
+                AsInvariant($@"The value {FormatValue(value.Value)} is not defined in the enumeration {typeof(TEnum).GetFullName().ToUIString()}."));
+        }
+    }
+}
