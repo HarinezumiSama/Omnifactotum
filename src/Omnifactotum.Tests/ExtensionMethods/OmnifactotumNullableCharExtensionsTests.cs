6c4c20c47ae0d5c06e6f7aef32b1e3dab371d77f
Vitalii Maklai <vitalii.maklai@gmail.com>
2024-08-28 16:47:43 -0400

    Added `OmnifactotumNullableCharExtensions` with `ToUIString(this char? value)`

---
+++ src/Omnifactotum.Tests/ExtensionMethods/OmnifactotumNullableCharExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Omnifactotum.Tests.ExtensionMethods;
+
+[TestFixture(TestOf = typeof(OmnifactotumNullableCharExtensions))]
+internal sealed class OmnifactotumNullableCharExtensionsTests
+{
+    [Test]
+    [TestCase(null, "null")]
+    [TestCase('\x0000', "'\0'")]
+    [TestCase('\x0001', "'\x0001'")]
+    [TestCase('\x001F', "'\x001F'")]
+    [TestCase('\x0020', "'\x0020'")]
+    [TestCase('4', "'4'")]
+    [TestCase('.', "'.'")]
+    [TestCase('a', "'a'")]
+    [TestCase('Q', "'Q'")]
+    [TestCase('Ώ', "'Ώ'")]
+    [TestCase('β', "'β'")]
+    [TestCase('Ы', "'Ы'")]
+    [TestCase('ъ', "'ъ'")]
+    [TestCase('/', "'/'")]
+    [TestCase('≠', "'≠'")]
+    [TestCase('`', "'`'")]
+    [TestCase('"', "'\"'")]
+    [TestCase('«', "'«'")]
+    [TestCase('»', "'»'")]
+    [TestCase('\'', "''''")]
+    public void TestToUIString(char? value, string expectedResult) => Assert.That(() => value.ToUIString(), Is.EqualTo(expectedResult));
+}
---
+++ src/Omnifactotum/ExtensionMethods/OmnifactotumNullableCharExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System.Runtime.CompilerServices;
+using Omnifactotum;
+using Omnifactotum.Annotations;
+using PureAttribute = System.Diagnostics.Contracts.PureAttribute;
+
+//// ReSharper disable RedundantNullnessAttributeWithNullableReferenceTypes
+
+//// ReSharper disable once CheckNamespace :: Namespace is intentionally named so in order to simplify usage of extension methods
+namespace System;
+
+/// <summary>
+///     Contains extension methods for the <b>nullable</b> <see cref="char"/> structure.
+/// </summary>
+public static class OmnifactotumNullableCharExtensions
+{
+    /// <summary>
+    ///     Converts the specified nullable <see cref="char"/> value to its UI representation using
+    ///     <see cref="OmnifactotumCharExtensions.ToUIString"/> if the value is not <see langword="null"/>;
+    ///     otherwise, returns <see cref="OmnifactotumRepresentationConstants.NullValueRepresentation"/>.
+    /// </summary>
+    /// <param name="value">
+    ///     The nullable character value to convert.
+    /// </param>
+    /// <returns>
+    ///     A UI string representation of the specified nullable character.
+    /// </returns>
+    /// <example>
+    ///     <code>
+    /// <![CDATA[
+    ///         char? value0 = null;
+    ///         Console.WriteLine("Value is {0}.", value0.ToUIString()); // Output: Value is null.
+    /// ]]>
+    ///     </code>
+    ///     <code>
+    /// <![CDATA[
+    ///         char? value1 = ' ';
+    ///         Console.WriteLine("Value is {0}.", value1.ToUIString()); // Output: Value is ' '.
+    /// ]]>
+    ///     </code>
+    ///     <code>
+    /// <![CDATA[
+    ///         char? value2 = 'H';
+    ///         Console.WriteLine("Value is {0}.", value2.ToUIString()); // Output: Value is 'H'.
+    /// ]]>
+    ///     </code>
+    ///     <code>
+    /// <![CDATA[
+    ///         char? value3 = '\'';
+    ///         Console.WriteLine("Value is {0}.", value3.ToUIString()); // Output: Value is ''''.
+    /// ]]>
+    ///     </code>
+    /// </example>
+    [Pure]
+    [Omnifactotum.Annotations.Pure]
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Standard)]
+    [NotNull]
+    public static string ToUIString(this char? value)
+        => value is null ? OmnifactotumRepresentationConstants.NullValueRepresentation : value.Value.ToUIString();
+}
