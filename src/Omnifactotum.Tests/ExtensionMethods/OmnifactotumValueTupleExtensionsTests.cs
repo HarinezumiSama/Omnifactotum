d5eb2b75088da55a8723c64183c08529b7c80165
Vitalii Maklai <vitalii.maklai@gmail.com>
2025-08-19 15:56:45 -0400

    Added `OmnifactotumValueTupleExtensions` with `ToKeyValuePair<TKey, TValue>()` and `ToDictionaryEntry<TKey, TValue>()`

---
+++ src/Omnifactotum.Tests/ExtensionMethods/OmnifactotumValueTupleExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+using Omnifactotum.ExtensionMethods;
+
+namespace Omnifactotum.Tests.ExtensionMethods;
+
+[TestFixture(TestOf = typeof(OmnifactotumValueTupleExtensions))]
+internal sealed class OmnifactotumValueTupleExtensionsTests
+{
+    [Test]
+    [TestCase(23, "value1")]
+    [TestCase("value2", -17)]
+    public void TestToKeyValuePair<TKey, TValue>(TKey key, TValue value)
+    {
+        var pair = ValueTuple.Create(key, value).ToKeyValuePair();
+        Assert.That(pair.Key, Is.EqualTo(key));
+        Assert.That(pair.Value, Is.EqualTo(value));
+    }
+
+    [Test]
+    [TestCase(-23, "value3")]
+    [TestCase("value4", 17)]
+    public void TestToDictionaryEntry<TKey, TValue>(TKey key, TValue value)
+        where TKey : notnull
+    {
+        var entry = ValueTuple.Create(key, value).ToDictionaryEntry();
+        Assert.That(entry.Key, Is.EqualTo(key));
+        Assert.That(entry.Value, Is.EqualTo(value));
+    }
+}
---
+++ src/Omnifactotum/ExtensionMethods/OmnifactotumValueTupleExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+
+namespace Omnifactotum.ExtensionMethods;
+
+/// <summary>
+///     Contains extension methods for the value tuple types.
+/// </summary>
+public static class OmnifactotumValueTupleExtensions
+{
+    /// <summary>
+    ///     Converts the specified <see cref="ValueTuple{TKey,TValue}"/> to <see cref="KeyValuePair{TKey,TValue}"/>.
+    /// </summary>
+    /// <param name="tuple">
+    ///     The value tuple to convert.
+    /// </param>
+    /// <typeparam name="TKey">
+    ///     The type of the value tuple's first element and the key in <see cref="KeyValuePair{TKey,TValue}"/>.
+    /// </typeparam>
+    /// <typeparam name="TValue">
+    ///     The type of the value tuple's second element and the value in <see cref="KeyValuePair{TKey,TValue}"/>.
+    /// </typeparam>
+    /// <returns>
+    ///     A <see cref="KeyValuePair{TKey,TValue}"/> created from <see cref="ValueTuple{TKey,TValue}"/>.
+    /// </returns>
+    [Pure]
+    [Omnifactotum.Annotations.Pure]
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Maximum)]
+    public static KeyValuePair<TKey, TValue> ToKeyValuePair<TKey, TValue>(this ValueTuple<TKey, TValue> tuple) => new(tuple.Item1, tuple.Item2);
+
+    /// <summary>
+    ///     Converts the specified <see cref="ValueTuple{TKey,TValue}"/> to <see cref="DictionaryEntry"/>.
+    /// </summary>
+    /// <param name="tuple">
+    ///     The value tuple to convert.
+    /// </param>
+    /// <typeparam name="TKey">
+    ///     The type of the value tuple's first element and the key in <see cref="DictionaryEntry"/>.
+    /// </typeparam>
+    /// <typeparam name="TValue">
+    ///     The type of the value tuple's second element and the value in <see cref="DictionaryEntry"/>.
+    /// </typeparam>
+    /// <returns>
+    ///     A <see cref="DictionaryEntry"/> created from <see cref="ValueTuple{TKey,TValue}"/>.
+    /// </returns>
+    [Pure]
+    [Omnifactotum.Annotations.Pure]
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Maximum)]
+    public static DictionaryEntry ToDictionaryEntry<TKey, TValue>(this ValueTuple<TKey, TValue> tuple)
+        where TKey : notnull
+        => new(tuple.Item1, tuple.Item2);
+}
