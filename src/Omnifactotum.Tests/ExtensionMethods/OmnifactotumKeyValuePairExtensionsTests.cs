5423a9cd0ddb22f525908ea8da57ebdf3ac3d2a9
Vitalii Maklai <vitalii.maklai@gmail.com>
2025-08-19 15:57:17 -0400

    Added `OmnifactotumKeyValuePairExtensions` with `ToValueTuple<TKey, TValue>()`

---
+++ src/Omnifactotum.Tests/ExtensionMethods/OmnifactotumKeyValuePairExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Omnifactotum.ExtensionMethods;
+
+namespace Omnifactotum.Tests.ExtensionMethods;
+
+[TestFixture(TestOf = typeof(OmnifactotumKeyValuePairExtensions))]
+internal sealed class OmnifactotumKeyValuePairExtensionsTests
+{
+    [Test]
+    [TestCase(19, "value1")]
+    [TestCase("value2", -13)]
+    public void TestToKeyValuePair<TKey, TValue>(TKey key, TValue value)
+    {
+        var pair = KeyValuePair.Create(key, value).ToValueTuple();
+        Assert.That(pair.Item1, Is.EqualTo(key));
+        Assert.That(pair.Item2, Is.EqualTo(value));
+    }
+}
---
+++ src/Omnifactotum/ExtensionMethods/OmnifactotumKeyValuePairExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+
+namespace Omnifactotum.ExtensionMethods;
+
+/// <summary>
+///     Contains extension methods for the <see cref="KeyValuePair{TKey,TValue}"/> structure.
+/// </summary>
+public static class OmnifactotumKeyValuePairExtensions
+{
+    /// <summary>
+    ///     Converts the specified <see cref="KeyValuePair{TKey,TValue}"/> to <see cref="ValueTuple{TKey,TValue}"/>.
+    /// </summary>
+    /// <param name="pair">
+    ///     The key/value pair to convert.
+    /// </param>
+    /// <typeparam name="TKey">
+    ///     The type of the key in <see cref="KeyValuePair{TKey,TValue}"/> and the value tuple's first element.
+    /// </typeparam>
+    /// <typeparam name="TValue">
+    ///     The type of the  value in <see cref="KeyValuePair{TKey,TValue}"/> and the value tuple's second element.
+    /// </typeparam>
+    /// <returns>
+    ///     A <see cref="ValueTuple{TKey,TValue}"/> created from <see cref="KeyValuePair{TKey,TValue}"/>.
+    /// </returns>
+    [Pure]
+    [Omnifactotum.Annotations.Pure]
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Maximum)]
+    public static ValueTuple<TKey, TValue> ToValueTuple<TKey, TValue>(this KeyValuePair<TKey, TValue> pair) => new(pair.Key, pair.Value);
+}
