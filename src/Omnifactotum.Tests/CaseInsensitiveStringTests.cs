b31c4f52107a47831aca89e1714103a8a3ae3fb2
Vitalii Maklai <vitalii.maklai@gmail.com>
2025-11-24 18:20:37 -0500

    [Breaking changes] `CaseInsensitiveString` improvements: Now a null string corresponds to a null `CaseInsensitiveString` and the underlying string value cannot be null

--- src/Omnifactotum.Tests/CaseInsensitiveStringTests.cs
+++ src/Omnifactotum.Tests/CaseInsensitiveStringTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
 using Omnifactotum.NUnit;
 
 namespace Omnifactotum.Tests;
@@ -7,11 +8,22 @@ namespace Omnifactotum.Tests;
 internal sealed class CaseInsensitiveStringTests
 {
     [Test]
-    [TestCase(null)]
+    public void TestParameterlessConstruction()
+    {
+        Assert.That(() => new CaseInsensitiveString().Value, Is.EqualTo(string.Empty));
+        Assert.That(() => default(CaseInsensitiveString).Value, Is.EqualTo(string.Empty));
+        Assert.That(() => (new CaseInsensitiveString[1])[0].Value, Is.EqualTo(string.Empty));
+    }
+
+    [Test]
+    public void TestConstructionWhenInvalidArgumentThenThrows() => Assert.That(() => new CaseInsensitiveString(null!), Throws.ArgumentNullException);
+
+    [Test]
     [TestCase("")]
     [TestCase("qWerTy")]
     [TestCase("a53d7ef8a3ec403296F170342a9f70bf")]
-    public void TestConstruction(string? value)
+    [TestCase("Have a good day! Bonne journée ! 좋은 하루 보내세요! Гарного дня! Eigðu góðan dag!")]
+    public void TestConstructionWhenValidArgumentThenSucceeds(string value)
     {
         var testee = new CaseInsensitiveString(value);
         Assert.That(() => testee.Value, Is.EqualTo(value));
@@ -29,11 +41,30 @@ internal sealed class CaseInsensitiveStringTests
     [TestCase("Have a good day! Bonne journée ! 좋은 하루 보내세요! Гарного дня! Eigðu góðan dag!")]
     public void TestConversionOperators(string? value)
     {
-        CaseInsensitiveString testee = value;
-        Assert.That(() => testee.Value, Is.EqualTo(value));
+        // Conversion to nullable
+        {
+            CaseInsensitiveString? testee = value;
+            Assert.That(() => testee?.Value, Is.EqualTo(value));
 
-        string? valueBack = testee;
-        Assert.That(valueBack, Is.EqualTo(value));
+            string? valueBack = testee;
+            Assert.That(valueBack, Is.EqualTo(value));
+        }
+
+        // Regular conversion
+        {
+            if (value is null)
+            {
+                Assert.That(() => (CaseInsensitiveString)value!, Throws.ArgumentNullException);
+            }
+            else
+            {
+                CaseInsensitiveString testee = value;
+                Assert.That(() => testee.Value, Is.EqualTo(value));
+
+                string valueBack = testee;
+                Assert.That(valueBack, Is.EqualTo(value));
+            }
+        }
     }
 
     [Test]
@@ -53,21 +84,32 @@ internal sealed class CaseInsensitiveStringTests
         "have A GoOD DAY! BONNe JoURNée ! 좋은 하루 보내세요! ГаРНоГо дНЯ! Eigðu gÓÐAn DaG!",
         "HAVE a gOod day! bonnE jOurnÉE ! 좋은 하루 보내세요! гАрнОгО Дня! eIGÐU GóðaN dAg!",
         AssertEqualityExpectation.EqualAndCannotBeSame)]
+    [SuppressMessage("ReSharper", "InvertIf")]
     public void TestEquality(string? value1, string? value2, AssertEqualityExpectation equalityExpectation)
     {
-        var testee1 = new CaseInsensitiveString(value1);
-        var testee2 = new CaseInsensitiveString(value2);
-        NUnitFactotum.AssertEquality(testee1, testee2, equalityExpectation, AssertEqualityOperatorExpectation.MustDefine);
+        // Nullable
+        {
+            var testee1 = CaseInsensitiveString.Create(value1);
+            var testee2 = CaseInsensitiveString.Create(value2);
+            NUnitFactotum.AssertEquality(testee1, testee2, equalityExpectation, AssertEqualityOperatorExpectation.MustDefine);
+        }
+
+        // Regular
+        if (value1 is not null && value2 is not null)
+        {
+            var testee1 = new CaseInsensitiveString(value1);
+            var testee2 = new CaseInsensitiveString(value2);
+            NUnitFactotum.AssertEquality(testee1, testee2, equalityExpectation, AssertEqualityOperatorExpectation.MustDefine);
+        }
     }
 
     [Test]
-    [TestCase(null)]
     [TestCase("")]
     [TestCase("\t")]
     [TestCase("Hello-World")]
     [TestCase("fbb097b992d144E1AC898e18a7d1ba39")]
     [TestCase("Have a good day! Bonne journée ! 좋은 하루 보내세요! Гарного дня! Eigðu góðan dag!")]
-    public void TestToString(string? value)
+    public void TestToString(string value)
     {
         var testee = new CaseInsensitiveString(value);
         Assert.That(() => testee.ToString(), Is.EqualTo(value));
--- src/Omnifactotum/CaseInsensitiveString.cs
+++ src/Omnifactotum/CaseInsensitiveString.cs
@@ -1,16 +1,27 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
-using Omnifactotum.Annotations;
+using CanBeNullAttribute = Omnifactotum.Annotations.CanBeNullAttribute;
+using NotNullAttribute = Omnifactotum.Annotations.NotNullAttribute;
+using PublicAPIAttribute = Omnifactotum.Annotations.PublicAPIAttribute;
 
 #if NET7_0_OR_GREATER
 using System.Numerics;
 #endif
+
+//// ReSharper disable RedundantNullnessAttributeWithNullableReferenceTypes
+//// ReSharper disable AnnotationRedundancyInHierarchy
 
 namespace Omnifactotum;
 
 /// <summary>
 ///     Represents the case-insensitive string.
 /// </summary>
+/// <remarks>
+///     For un uninitialized <see cref="CaseInsensitiveString"/> instance (as in <c>var s = default(CaseInsensitiveString)</c>),
+///     <see cref="Value"/> is <see cref="string.Empty"/>.
+/// </remarks>
+/// <seealso cref="CaseInsensitiveString.Comparer"/>
 public readonly struct CaseInsensitiveString
     :
 #if NET7_0_OR_GREATER
@@ -24,18 +35,43 @@ public readonly struct CaseInsensitiveString
     [PublicAPI]
     public static readonly StringComparer Comparer = StringComparer.OrdinalIgnoreCase;
 
+    private readonly string _value;
+
     /// <summary>
     ///     Initializes a new instance of the <see cref="CaseInsensitiveString"/> structure.
     /// </summary>
     /// <param name="value">
     ///     The original string value to create a <see cref="CaseInsensitiveString"/> for.
     /// </param>
-    public CaseInsensitiveString(string? value) => Value = value;
+    /// <exception cref="ArgumentNullException">
+    ///     <paramref name="value"/> is <see langword="null"/>.
+    /// </exception>
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Maximum)]
+    public CaseInsensitiveString([NotNull] string value) => _value = value ?? throw new ArgumentNullException(nameof(value));
 
     /// <summary>
     ///     Gets the original string value.
     /// </summary>
-    public string? Value { get; }
+    /// <remarks>
+    ///     For un uninitialized <see cref="CaseInsensitiveString"/> instance (as in <c>var s = default(CaseInsensitiveString)</c>),
+    ///     <see cref="Value"/> is <see cref="string.Empty"/>.
+    /// </remarks>
+    [NotNull]
+    public string Value => _value ?? string.Empty;
+
+    /// <summary>
+    ///     Creates a nullable <see cref="CaseInsensitiveString"/> instance from the specified nullable string value.
+    /// </summary>
+    /// <param name="value">
+    ///     A nullable string value to create a nullable <see cref="CaseInsensitiveString"/> instance from.
+    /// </param>
+    /// <returns>
+    ///     A <see cref="CaseInsensitiveString"/> instance that has <see cref="Value"/> equal to the specified string value if it's not <see langword="null"/>;
+    ///     or <see langword="null"/> if the specified string value is <see langword="null"/>.
+    /// </returns>
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Maximum)]
+    [return: NotNullIfNotNull(nameof(value))]
+    public static implicit operator CaseInsensitiveString?([CanBeNull] string? value) => Create(value);
 
     /// <summary>
     ///     Creates a <see cref="CaseInsensitiveString"/> instance from the specified string value.
@@ -46,7 +82,11 @@ public readonly struct CaseInsensitiveString
     /// <returns>
     ///     A <see cref="CaseInsensitiveString"/> instance that has <see cref="Value"/> equal to the specified string value.
     /// </returns>
-    public static implicit operator CaseInsensitiveString(string? value) => new(value);
+    /// <exception cref="ArgumentNullException">
+    ///     <paramref name="value"/> is <see langword="null"/>.
+    /// </exception>
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Maximum)]
+    public static implicit operator CaseInsensitiveString([NotNull] string value) => new(value);
 
     /// <summary>
     ///     Returns the value of the <see cref="Value"/> property.
@@ -57,7 +97,24 @@ public readonly struct CaseInsensitiveString
     /// <returns>
     ///     The value of the <see cref="Value"/> property.
     /// </returns>
-    public static implicit operator string?(CaseInsensitiveString obj) => obj.Value;
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Maximum)]
+    [NotNull]
+    public static implicit operator string(CaseInsensitiveString obj) => obj.Value;
+
+    /// <summary>
+    ///     Returns the value of the <see cref="Value"/> property if <paramref name="obj"/> is not <see langword="null"/>;
+    ///     otherwise, returns<see langword="null"/>.
+    /// </summary>
+    /// <param name="obj">
+    ///     A nullable <see cref="CaseInsensitiveString"/> instance to convert to string.
+    /// </param>
+    /// <returns>
+    ///     The value of the <see cref="Value"/> property or <see langword="null"/>.
+    /// </returns>
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Maximum)]
+    [CanBeNull]
+    [return: NotNullIfNotNull(nameof(obj))]
+    public static implicit operator string?(CaseInsensitiveString? obj) => obj?.Value;
 
     /// <summary>
     ///     Determines whether the two specified <see cref="CaseInsensitiveString"/> instances are equal.
@@ -89,13 +146,61 @@ public readonly struct CaseInsensitiveString
     [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Maximum)]
     public static bool operator !=(CaseInsensitiveString left, CaseInsensitiveString right) => !Equals(left, right);
 
-    /// <inheritdoc />
-    public override bool Equals(object? obj) => obj is CaseInsensitiveString castObj && Equals(castObj);
+    /// <summary>
+    ///     Determines whether the two specified nullable <see cref="CaseInsensitiveString"/> instances are equal.
+    /// </summary>
+    /// <param name="left">
+    ///     The first nullable <see cref="CaseInsensitiveString"/> instance to compare.
+    /// </param>
+    /// <param name="right">
+    ///     The second nullable <see cref="CaseInsensitiveString"/> instance to compare.
+    /// </param>
+    /// <returns>
+    ///     <see langword="true"/> if the two specified nullable <see cref="CaseInsensitiveString"/> instances are equal;
+    ///     otherwise, <see langword="false"/>.
+    /// </returns>
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Maximum)]
+    public static bool operator ==(CaseInsensitiveString? left, CaseInsensitiveString? right) => Equals(left, right);
+
+    /// <summary>
+    ///     Determines whether the two specified nullable <see cref="CaseInsensitiveString"/> instances are not equal.
+    /// </summary>
+    /// <param name="left">
+    ///     The first nullable <see cref="CaseInsensitiveString"/> instance to compare.
+    /// </param>
+    /// <param name="right">
+    ///     The second nullable <see cref="CaseInsensitiveString"/> instance to compare.
+    /// </param>
+    /// <returns>
+    ///     <see langword="true"/> if the two specified nullable <see cref="CaseInsensitiveString"/> instances are not equal;
+    ///     otherwise, <see langword="false"/>.
+    /// </returns>
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Maximum)]
+    public static bool operator !=(CaseInsensitiveString? left, CaseInsensitiveString? right) => !Equals(left, right);
+
+    /// <summary>
+    ///     Creates a nullable <see cref="CaseInsensitiveString"/> instance from the specified nullable string value.
+    /// </summary>
+    /// <param name="value">
+    ///     A nullable string value to create a nullable <see cref="CaseInsensitiveString"/> instance from.
+    /// </param>
+    /// <returns>
+    ///     A <see cref="CaseInsensitiveString"/> instance that has <see cref="Value"/> equal to the specified string value if it's not <see langword="null"/>;
+    ///     or <see langword="null"/> if the specified string value is <see langword="null"/>.
+    /// </returns>
+    [return: NotNullIfNotNull(nameof(value))]
+    public static CaseInsensitiveString? Create([CanBeNull] string? value) => value is null ? (CaseInsensitiveString?)null : new CaseInsensitiveString(value);
 
     /// <inheritdoc />
-    public override int GetHashCode() => Value is null ? 0 : Comparer.GetHashCode(Value);
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Maximum)]
+    public override bool Equals([CanBeNull] object? obj) => obj is CaseInsensitiveString castObj && Equals(castObj);
 
     /// <inheritdoc />
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Maximum)]
+    public override int GetHashCode() => Comparer.GetHashCode(Value);
+
+    /// <inheritdoc />
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Maximum)]
     public bool Equals(CaseInsensitiveString other) => Equals(this, other);
 
     /// <summary>
@@ -104,8 +209,26 @@ public readonly struct CaseInsensitiveString
     /// <returns>
     ///     The value of the <see cref="Value"/> property.
     /// </returns>
-    public override string? ToString() => Value;
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Maximum)]
+    [NotNull]
+    public override string ToString() => Value;
 
     [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Maximum)]
     private static bool Equals(CaseInsensitiveString left, CaseInsensitiveString right) => Comparer.Equals(left.Value, right.Value);
+
+    [MethodImpl(OmnifactotumConstants.MethodOptimizationOptions.Maximum)]
+    private static bool Equals(CaseInsensitiveString? left, CaseInsensitiveString? right)
+    {
+        if (left is null && right is null)
+        {
+            return true;
+        }
+
+        if (left is null || right is null)
+        {
+            return false;
+        }
+
+        return Equals(left.Value, right.Value);
+    }
 }
